3d0ac4f09210ba98af4b50ffc8cf35872931d522
Braden Wong <bmw02002turbo@gmail.com>
2022-10-22 09:28:42 -0400

    Feat: Simplify and input

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,34 +1,35 @@
-<script lang="ts">
-  const photoNames = [
-    'LocationsTable.png',
-    'LocationsList.png',
-    'Map.png',
-    'Profile.png',
-    'Profile Other.png',
-    'New Haven.png',
-    'New Haven In.png',
-  ];
-
-  const photoURLs = [
-    'https://i.imgur.com/O7KS8N5.png',
-    'https://i.imgur.com/8VIWiMd.png',
-    'https://i.imgur.com/63FnRQX.png',
-    'https://i.imgur.com/eyq1wEn.png',
-    'https://i.imgur.com/tVZGAKh.png',
-    'https://i.imgur.com/PzEQtAG.png',
-  ];
-</script>
+<script lang="ts"></script>
 
 <div class="min-h-screen-nav hero mx-auto max-w-md bg-base-100 text-base-content md:max-w-full">
-  <div class="hero-overlay bg-opacity-60 h-screen" />
-  <div class="hero-content px-4 text-center md:px-0">
-    <div>
-      <h2 class="mt-32 mb-2 text-4xl font-extrabold md:text-6xl">Yale Course Keyword Search</h2>
-      <h3 class="mb-5 text-3xl font-bold">A search box that allows you to search coursetable reviews by keyword</h3>
-      <p class="mx-auto w-full max-w-xl">Start by typing in this box:</p>
-      <div class="mb-36 flex w-full justify-center">
-      </div>
-    </div>
-  </div>
+	<div class="hero-overlay h-screen bg-opacity-60" />
+	<div class="hero-content px-4 text-center md:px-0">
+		<div>
+			<h2 class="mt-32 mb-2 text-4xl font-extrabold md:text-6xl">Yale Course Keyword Search</h2>
+			<h3 class="mb-5 text-3xl font-bold">
+				A search box that allows you to search coursetable reviews by keyword
+			</h3>
+			<p class="mx-auto w-full max-w-xl">Start by typing in this box:</p>
+			<div class="form-control">
+				<div class="input-group">
+					<input type="text" placeholder="Search…" class="input input-bordered" />
+					<button class="btn btn-square">
+						<svg
+							xmlns="http://www.w3.org/2000/svg"
+							class="h-6 w-6"
+							fill="none"
+							viewBox="0 0 24 24"
+							stroke="currentColor"
+						>
+							<path
+								stroke-linecap="round"
+								stroke-linejoin="round"
+								stroke-width="2"
+								d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
+							/></svg
+						>
+					</button>
+				</div>
+			</div>
+		</div>
+	</div>
 </div>
-
