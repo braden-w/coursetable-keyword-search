c3aabeda7b216531d15d915ef35ce2a64c9877c6
Braden Wong <bmw02002turbo@gmail.com>
2022-10-22 09:37:09 -0400

    Feat: Search Button

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -11,8 +11,11 @@
 			<!-- <p class="">Start by typing in this box:</p> -->
 			<div class="form-control">
 				<div class="input-group">
-					<input type="text" placeholder="Search…" class="input input-bordered" />
-					<button class="btn btn-square">
+					<span>Search course reviews with</span>
+					<input type="text" placeholder="Keyword..." class="input input-bordered" />
+					<span>for courses that start with</span>
+					<input type="text" placeholder="ECON, CPSC, PLSC..." class="input input-bordered" />
+					<button class="btn">
 						<svg
 							xmlns="http://www.w3.org/2000/svg"
 							class="h-6 w-6"
@@ -25,8 +28,9 @@
 								stroke-linejoin="round"
 								stroke-width="2"
 								d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
-							/></svg
-						>
+							/>
+						</svg>
+						Search
 					</button>
 				</div>
 			</div>
