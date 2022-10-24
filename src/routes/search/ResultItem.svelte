27e2f57d7afa2f52e818e330e8242037b0d2a5cc
Braden Wong <bmw02002turbo@gmail.com>
2022-10-24 00:28:09 -0400

    Chore

--- src/routes/search/ResultItem.svelte
+++ src/routes/search/ResultItem.svelte
@@ -1,5 +1,5 @@
 <script lang="ts">
-	import { slide, fly } from 'svelte/transition';
+	import { slide } from 'svelte/transition';
 	import { ChevronDown, ChevronRight, ChevronUp } from '@steeze-ui/heroicons';
 	import { Icon } from '@steeze-ui/svelte-icon';
 	import type { SearchResponse } from '$lib/types/SearchResponse';
