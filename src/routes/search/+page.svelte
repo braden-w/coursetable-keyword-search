5160666df24ce691c6cdc6a96f6006d50670f6ca
Braden Wong <bmw02002turbo@gmail.com>
2022-10-23 22:03:01 -0400

    Refactor: ResultItem

--- src/routes/search/+page.svelte
+++ src/routes/search/+page.svelte
@@ -1,5 +1,6 @@
 <script lang="ts">
-	import { ChevronRight, MagnifyingGlass } from '@steeze-ui/heroicons';
+	import ResultItem from './ResultItem.svelte';
+	import { MagnifyingGlass } from '@steeze-ui/heroicons';
 	import { Icon } from '@steeze-ui/svelte-icon';
 	import type { SearchResponse } from 'src/routes/search/[keyword]/[course_keyword]/types';
 	let keyword = '';
@@ -179,45 +180,7 @@
 	<div class="overflow-hidden bg-white shadow sm:rounded-md">
 		<ul role="list" class="divide-y divide-gray-200">
 			{#each courses as course}
-				<li>
-					<a href="#" class="block hover:bg-gray-50">
-						<div class="flex items-center px-4 py-4 sm:px-6">
-							<div class="min-w-0 flex-1 sm:flex sm:items-center sm:justify-between">
-								<div>
-									<div class="flex text-sm">
-										<p class="truncate font-medium text-primary">
-											{course.computed_listing_infos_aggregate.nodes[0].all_course_codes}
-											{course.title}
-										</p>
-										<p class="ml-1 flex-shrink-0 font-normal text-gray-500">
-											<!-- in {course.description} -->
-										</p>
-									</div>
-									<div class="mt-2 flex">
-										<div class="flex items-center text-sm text-gray-500">
-											{course.description}
-										</div>
-									</div>
-								</div>
-								<div class="mt-4 flex-shrink-0 sm:mt-0 sm:ml-5">
-									<div class="flex -space-x-1 overflow-hidden">
-										<!-- <img v-for="applicant in position.applicants" :key="applicant.email" class="inline-block h-6 w-6 rounded-full ring-2 ring-white" :src="applicant.imageUrl" :alt="applicant.name" /> -->
-									</div>
-								</div>
-							</div>
-							<div class="ml-5 flex-shrink-0">
-								<Icon src={ChevronRight} class="h-5 w-5 text-gray-400" aria-hidden="true" />
-							</div>
-						</div>
-					</a>
-				</li>
-				{#each course.evaluation_narratives as review}
-					<li>
-						<div class="px-4 py-4 sm:px-6">
-							<p class="text-sm text-gray-500">{review.comment}</p>
-						</div>
-					</li>
-				{/each}
+				<ResultItem {course} />
 			{/each}
 		</ul>
 	</div>
