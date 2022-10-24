856522c9a07b2a10ba3194b7f49c229b87a30cc5
Braden Wong <bmw02002turbo@gmail.com>
2022-10-24 01:18:43 -0400

    Feat: New Query

--- src/routes/api/search/graphql.ts
+++ src/routes/api/search/graphql.ts
@@ -1,54 +1,43 @@
-const query = `query searchCoursesByKeyword ($keyword: String!, $course_keyword: String!) {
-   courses (
-    where: {
-     _and: [
-      {
-       evaluation_narratives: {
-        comment: {
-         _ilike: $keyword
-        }
-       }
-      },
-      {
-       course: {
-        computed_listing_infos: {
-         course_code: {
-          _ilike: $course_keyword
-         }
-        }
-       }
-      }
-     ]
-    }
-   )
-   {
-    description
-    title
-    areas
-    average_rating
-    average_rating_same_professors
-    average_workload
-    average_workload_same_professors
-    computed_listing_infos_aggregate {
-     nodes {
-      all_course_codes
-      areas
-      average_gut_rating
-      average_professor
-      course_code
-      crn
-     }
-    }
-    course_id
-    description
-    locations_summary
-    requirements
-    skills
-    evaluation_narratives {
-     comment
-    }
-   }
-  }`;
+const query = `query searchCoursesByKeyword($keyword: String!, $course_keyword: String!) {
+	courses(
+		where: {
+			_and: [
+				{ evaluation_narratives: { comment: { _ilike: $keyword } } }
+				{
+					course: {
+						computed_listing_infos: { course_code: { _ilike: $course_keyword } }
+					}
+				}
+			]
+		}
+	) {
+		description
+		title
+		areas
+		average_rating
+		average_rating_same_professors
+		average_workload
+		average_workload_same_professors
+		computed_listing_infos_aggregate {
+			nodes {
+				all_course_codes
+				areas
+				average_gut_rating
+				average_professor
+				course_code
+				crn
+			}
+		}
+		course_id
+		description
+		locations_summary
+		requirements
+		skills
+		evaluation_narratives(where: { comment: { _ilike: $keyword } }) {
+			comment
+		}
+	}
+}`;
 export const graphQL = ({
 	keyword,
 	course_keyword
