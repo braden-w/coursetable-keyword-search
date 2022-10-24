849cda8a8f7d7c30d348241509d9cd79269a3a9f
Braden Wong <bmw02002turbo@gmail.com>
2022-10-24 18:08:56 -0400

    Feat: Working Button Groups

--- src/lib/suggested queries/QueriesRow.svelte
+++ src/lib/suggested queries/QueriesRow.svelte
@@ -52,8 +52,20 @@
 	];
 </script>
 
-<div class="flex gap-2 overflow-x-auto">
-	{#each queries as query}
+<span class="isolate inline-flex rounded-md shadow-sm">
+	<button
+		type="button"
+		class="relative inline-flex items-center rounded-l-md border border-gray-300 bg-white px-4 py-2 text-sm font-medium text-gray-700 hover:bg-gray-50 focus:z-10 focus:border-indigo-500 focus:outline-none focus:ring-1 focus:ring-indigo-500"
+	>
+		Years
+	</button>
+
+	{#each queries.slice(1, -1) as query}
+		<button
+			type="button"
+			class="relative -ml-px inline-flex items-center border border-gray-300 bg-white px-4 py-2 text-sm font-medium text-gray-700 hover:bg-gray-50 focus:z-10 focus:border-indigo-500 focus:outline-none focus:ring-1 focus:ring-indigo-500"
+			>Months</button
+		>
 		<a
 			href="/search?{new URLSearchParams({
 				keyword: query.keyword,
@@ -63,4 +75,10 @@
 			<QueryButton {...query} />
 		</a>
 	{/each}
-</div>
+			<button
+			type="button"
+			class="relative -ml-px inline-flex items-center rounded-r-md border border-gray-300 bg-white px-4 py-2 text-sm font-medium text-gray-700 hover:bg-gray-50 focus:z-10 focus:border-indigo-500 focus:outline-none focus:ring-1 focus:ring-indigo-500"
+			>Days</button
+		>
+
+</span>
--- src/lib/suggested queries/QueryButton.svelte
+++ src/lib/suggested queries/QueryButton.svelte
@@ -4,18 +4,3 @@
 	export let count: string;
 </script>
 
-<button
-	id={keyword}
-	name={keyword}
-	class="inline-flex rounded-md text-sm shadow-sm hover:bg-gray-50 focus:border-indigo-500 focus:outline-none focus:ring-1 focus:ring-indigo-500"
->
-	<span
-		class="-mr-px rounded-l-md border border-gray-300 bg-white px-3 py-2 pr-6 font-medium text-gray-700"
-	>
-		{title}
-	</span>
-	<span class="rounded-r-md border border-gray-300 bg-white px-2 py-2">
-		<p>{count}</p>
-	</span>
-	<label for={keyword} class="sr-only">Select keyword</label>
-</button>
