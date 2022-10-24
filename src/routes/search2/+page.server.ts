034e47d24324f827b2e96bec73e39924c9d08e43
Braden Wong <bmw02002turbo@gmail.com>
2022-10-24 17:51:59 -0400

    Feat: Delete old files

--- src/routes/search2/+page.server.ts
+++
@@ -1,9 +0,0 @@
-import { queryCourseTable } from '../api/search/+server';
-import type { PageServerLoad } from './$types';
-
-export const load: PageServerLoad = async ({ url }: { url: URL }) => {
-	const keyword = url.searchParams.get('keyword') ?? '%';
-	const course_keyword = url.searchParams.get('course_keyword') ?? '%';
-	const response = await queryCourseTable({ keyword, course_keyword });
-	return { response };
-};
