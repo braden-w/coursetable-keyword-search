a07a0f6701d4ef1e9efd9b40f8f95ecad8df8fec
Braden Wong <bmw02002turbo@gmail.com>
2022-10-24 14:59:03 -0400

    Feat: Header

--- src/lib/Header.svelte
+++ src/lib/Header.svelte
@@ -1,23 +1,15 @@
 <script lang="ts">
 	import Logo from './Logo.svelte';
-	import { page } from '$app/stores';
 
 	import {
 		Disclosure,
 		DisclosureButton,
-		DisclosurePanel,
-		Menu,
-		MenuButton,
-		MenuItem,
-		MenuItems
 	} from '@rgossiaux/svelte-headlessui';
 	import { Icon } from '@steeze-ui/svelte-icon';
-	import { Bars3, MagnifyingGlass, XMark, BellAlert, Bell } from '@steeze-ui/heroicons';
-
-	const navigation = [{ name: 'Search', href: '/search' }];
+	import { Bars3, XMark } from '@steeze-ui/heroicons';
 </script>
 
-<Disclosure as="nav" class="bg-neutral" let:open>
+<Disclosure as="nav" class="" let:open>
 	<div class="mx-auto max-w-7xl px-2 sm:px-4 lg:px-8">
 		<div class="relative flex h-16 items-center justify-between">
 			<div class="flex items-center px-2 lg:px-0">
@@ -27,21 +19,9 @@
 						<Logo class="hidden h-8 w-auto lg:block" />
 					</a>
 				</div>
-				<div class="hidden lg:ml-6 lg:block">
-					<div class="flex space-x-4">
-						<!-- Current: "bg-gray-900 text-white", Default: "text-gray-300 hover:bg-gray-700 hover:text-white" -->
-						{#each navigation as item}
-							<a
-								href={item.href}
-								class={$page.url.pathname === item.href
-									? 'rounded-md bg-gray-900 px-3 py-2 text-sm font-medium text-white'
-									: 'rounded-md px-3 py-2 text-sm font-medium text-gray-300 hover:bg-gray-700 hover:text-white'}
-							>
-								{item.name}
-							</a>
-						{/each}
-					</div>
-				</div>
+				<a href="/">
+					<h1 class="ml-4 hidden text-4xl font-extrabold lg:block">Yale Course Keyword Search</h1>
+				</a>
 			</div>
 			<div class="flex lg:hidden">
 				<!-- Mobile menu button -->
@@ -59,17 +39,4 @@
 		</div>
 	</div>
 
-	<DisclosurePanel class="lg:hidden">
-		<div class="space-y-1 px-2 pt-2 pb-3">
-			<!-- Current: "bg-gray-900 text-white", Default: "text-gray-300 hover:bg-gray-700 hover:text-white" -->
-			{#each navigation as item}
-				<DisclosureButton
-					as="a"
-					href={item.href}
-					class="block rounded-md px-3 py-2 text-base font-medium text-gray-300 hover:bg-gray-700 hover:text-white"
-					>{item.name}
-				</DisclosureButton>
-			{/each}
-		</div>
-	</DisclosurePanel>
 </Disclosure>
