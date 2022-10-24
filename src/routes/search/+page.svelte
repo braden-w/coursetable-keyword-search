d64b5bf3c88353940964d87d9f5027f9fe2b2b1d
Braden Wong <bmw02002turbo@gmail.com>
2022-10-23 20:55:07 -0400

    Feat: Working submit and search

--- src/routes/search/+page.svelte
+++ src/routes/search/+page.svelte
@@ -1,25 +1,44 @@
 <script lang="ts">
 	import { MagnifyingGlass } from '@steeze-ui/heroicons';
 	import { Icon } from '@steeze-ui/svelte-icon';
+	import type { SearchResponse } from 'src/routes/search/[keyword]/[course_keyword]/types';
 	let keyword = '';
 	let courseKeyword = '';
 
 	$: keywordWithPercents = `%${keyword}%`;
 	$: courseKeywordWithPercents = `%${courseKeyword}%`;
 
+	const onKeydown = (e: KeyboardEvent) => {
+		if (e.key === 'Enter') {
+			onSubmit();
+		}
+	};
+
+	let courses: SearchResponse['data']['courses'] = [];
+
 	const onSubmit = async () => {
+		console.log(
+			'🚀 ~ file: +page.svelte ~ line 25 ~ onSubmit ~ courseKeywordWithPercents',
+			courseKeywordWithPercents
+		);
+		console.log(
+			'🚀 ~ file: +page.svelte ~ line 25 ~ onSubmit ~ keywordWithPercents',
+			keywordWithPercents
+		);
 		// Send api request to search passing {keyword: keywordWithPercents, courseKeyword: courseKeywordWithPercents}
 		const response = await fetch(
 			`/api/search?keyword=${encodeURIComponent(
 				keywordWithPercents
 			)}&courseKeyword=${encodeURIComponent(courseKeywordWithPercents)}`
 		);
-		const data = await response.json();
+		const data = (await response.json()) as SearchResponse;
+		courses = data.data.courses;
+		console.log('🚀 ~ file: +page.svelte ~ line 18 ~ onSubmit ~ data', data);
 	};
 </script>
 
-<div class="hero bg-base-100 text-base-content min-h-screen">
-<!-- <div class="hero-overlay bg-opacity-60"></div> -->
+<div class="hero min-h-screen bg-base-100 text-base-content">
+	<!-- <div class="hero-overlay bg-opacity-60"></div> -->
 	<div class="hero-content">
 		<div>
 			<h2 class="mb-2 text-4xl font-extrabold md:text-6xl">Yale Course Keyword Search</h2>
@@ -39,9 +58,28 @@
 						class="block w-full rounded-md border border-gray-300 bg-white py-2 pl-10 pr-3 text-sm placeholder-gray-500 focus:border-indigo-500 focus:text-gray-900 focus:placeholder-gray-400 focus:outline-none focus:ring-1 focus:ring-indigo-500 sm:text-sm"
 						placeholder="Search by course keyword..."
 						type="search"
+						bind:value={keyword}
+						on:keydown={onKeydown}
 					/>
 				</div>
 			</div>
 		</div>
 	</div>
 </div>
+
+{#each courses as course}
+	<div class="card">
+		<div class="card-body">
+			<h2 class="card-title">
+				{course.computed_listing_infos_aggregate.nodes[0].all_course_codes}
+			</h2>
+			<h3 class="card-subtitle">{course.title}</h3>
+			<p class="card-text">{course.description}</p>
+		</div>
+		{#each course.evaluation_narratives as review}
+			<div class="card-body">
+				<p class="card-text">{review.comment}</p>
+			</div>
+		{/each}
+	</div>
+{/each}
