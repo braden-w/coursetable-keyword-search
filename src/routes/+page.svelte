56cc8e59c479f27c024bbdc50cac60be24adadb8
Braden Wong <bmw02002turbo@gmail.com>
2022-10-24 11:51:18 -0400

    Feat: Update index

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,9 +1,40 @@
 <script lang="ts">
 </script>
 
+<div class="relative">
+	<div class="absolute inset-x-0 bottom-0 h-1/2 bg-base-200" />
+	<div class="mx-auto max-w-7xl sm:px-6 lg:px-8">
+		<div class="relative shadow-xl sm:overflow-hidden sm:rounded-2xl">
+			<div class="absolute inset-0">
+				<div class="absolute inset-0 bg-indigo-700 mix-blend-multiply" />
+			</div>
+			<div class="relative px-4 py-16 sm:px-6 sm:py-24 lg:py-32 lg:px-8">
+				<h1 class="text-center text-4xl font-extrabold tracking-tight sm:text-5xl lg:text-6xl">
+					Yale Course Keyword Search
+				</h1>
+				<h2 class="mx-auto mt-6 text-center text-2xl font-bold max-w-lg sm:max-w-3xl">
+					A search box that allows you to search CourseTable reviews by keyword
+				</h2>
+				<div class="mx-auto mt-10 max-w-sm sm:flex sm:max-w-none sm:justify-center">
+					<div class="space-y-4 sm:mx-auto sm:inline-grid sm:grid-cols-2 sm:gap-5 sm:space-y-0">
+						<a
+							href="#"
+							class="flex items-center justify-center rounded-md border border-transparent bg-white px-4 py-3 text-base font-medium text-indigo-700 shadow-sm hover:bg-indigo-50 sm:px-8"
+							>Get started</a
+						>
+						<a
+							href="#"
+							class="flex items-center justify-center rounded-md border border-transparent bg-indigo-500 bg-opacity-60 px-4 py-3 text-base font-medium text-white shadow-sm hover:bg-opacity-70 sm:px-8"
+							>Live demo</a
+						>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
 <div class="hero bg-base-100 text-base-content">
-	<div class="hero-overlay h-screen bg-opacity-60" />
-	<div class="hero-content">
+	<div class="mt-64">
 		<div>
 			<h2 class="mb-2 text-4xl font-extrabold md:text-6xl">Yale Course Keyword Search</h2>
 			<h3 class="mb-4 text-3xl font-bold">
