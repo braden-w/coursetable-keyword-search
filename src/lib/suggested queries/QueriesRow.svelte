fff07677b05903fc33014ed34f4a84f8af66b7fe
Braden Wong <bmw02002turbo@gmail.com>
2022-10-24 18:20:21 -0400

    Feat: Button Row working

--- src/lib/suggested queries/QueriesRow.svelte
+++ src/lib/suggested queries/QueriesRow.svelte
@@ -4,13 +4,11 @@
 		{
 			title: '"Favorite" courses',
 			keyword: 'favorite',
-			course_keyword: '',
 			count: '1000'
 		},
 		{
 			title: '"Best" courses',
 			keyword: 'best',
-			course_keyword: '',
 			count: '1000'
 		},
 		{
@@ -40,7 +38,6 @@
 		{
 			title: '"Quintessential" history courses',
 			keyword: 'quintessential',
-			course_keyword: 'HIST',
 			count: '18'
 		},
 		{
@@ -61,19 +58,9 @@
 	</button>
 
 	{#each queries.slice(1, -1) as query}
-		<button
-			type="button"
-			class="relative -ml-px inline-flex items-center border border-gray-300 bg-white px-4 py-2 text-sm font-medium text-gray-700 hover:bg-gray-50 focus:z-10 focus:border-indigo-500 focus:outline-none focus:ring-1 focus:ring-indigo-500"
-			>Months</button
-		>
-		<a
-			href="/search?{new URLSearchParams({
-				keyword: query.keyword,
-				course_keyword: query.course_keyword
-			})}"
-		>
-			<QueryButton {...query} />
-		</a>
+
+	<QueryButton {...query} />
+
 	{/each}
 			<button
 			type="button"
--- src/lib/suggested queries/QueryButton.svelte
+++ src/lib/suggested queries/QueryButton.svelte
@@ -1,6 +1,21 @@
 <script lang="ts">
 	export let keyword: string;
+	export let course_keyword: string = '';
+	export let areas_skills_keyword: string = '';
 	export let title: string;
-	export let count: string;
 </script>
 
+<a
+	href="/search?{new URLSearchParams({
+		keyword: keyword,
+		course_keyword: course_keyword,
+		areas_skills_keyword: areas_skills_keyword
+	})}"
+>
+	<button
+		type="button"
+		class="relative -ml-px inline-flex items-center border border-gray-300 bg-white px-4 py-2 text-sm font-medium text-gray-700 hover:bg-gray-50 focus:z-10 focus:border-indigo-500 focus:outline-none focus:ring-1 focus:ring-indigo-500"
+	>
+		{title}
+	</button>
+</a>
