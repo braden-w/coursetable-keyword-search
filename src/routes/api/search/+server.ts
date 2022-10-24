73ee2e0df57bbf5c7c3f10b917d0ef3160426e60
Braden Wong <bmw02002turbo@gmail.com>
2022-10-23 23:55:28 -0400

    Refactor: searchResponse in API

--- src/routes/api/search/+server.ts
+++ src/routes/api/search/+server.ts
@@ -1,81 +1,92 @@
 import type { SearchResponse } from '../../search/[keyword]/[course_keyword]/types';
 import type { RequestHandler } from './$types';
 import { env } from '$env/dynamic/public';
-import {json} from '@sveltejs/kit';
+import { json } from '@sveltejs/kit';
 
-const query = ({ keyword, course_keyword }: { keyword: string; course_keyword: string }) => ({
-	method: 'POST',
-	headers: {
-		Cookie: env.PUBLIC_COURSETABLE_COOKIE,
-		origin: 'https://www.coursetable.com',
-		Referer: 'https://www.coursetable.com',
-		'Content-Type': 'application/json'
-	},
-	body: JSON.stringify({
-		query: `query searchCoursesByKeyword ($keyword: String!, $course_keyword: String!) {
-	courses (
-		where: {
-			_and: [
-				{
-					evaluation_narratives: {
-						comment: {
-							_ilike: $keyword
+const searchResponse = async ({
+	keyword,
+	course_keyword
+}: {
+	keyword: string;
+	course_keyword: string;
+}) => {
+	const options = {
+		method: 'POST',
+		headers: {
+			Cookie: env.PUBLIC_COURSETABLE_COOKIE,
+			origin: 'https://www.coursetable.com',
+			Referer: 'https://www.coursetable.com',
+			'Content-Type': 'application/json'
+		},
+		body: JSON.stringify({
+			query: `query searchCoursesByKeyword ($keyword: String!, $course_keyword: String!) {
+			courses (
+				where: {
+					_and: [
+						{
+							evaluation_narratives: {
+								comment: {
+									_ilike: $keyword
+								}
+							}
+						},
+						{
+							course: {
+								computed_listing_infos: {
+									course_code: {
+										_ilike: $course_keyword
+									}
+								}
+							}
 						}
-					}
-			},
+					]
+				}
+			)
 			{
-				course: {
-					computed_listing_infos: {
-						course_code: {
-							_ilike: $course_keyword
-						}
+				description
+				title
+				areas
+				average_rating
+				average_rating_same_professors
+				average_workload
+				average_workload_same_professors
+				computed_listing_infos_aggregate {
+					nodes {
+						all_course_codes
+						areas
+						average_gut_rating
+						average_professor
+						course_code
+						crn
 					}
 				}
-			}
-			]
-		}
-	)
-	{
-		description
-		title
-		areas
-		average_rating
-		average_rating_same_professors
-		average_workload
-		average_workload_same_professors
-		computed_listing_infos_aggregate {
-			nodes {
-				all_course_codes
-				areas
-				average_gut_rating
-				average_professor
-				course_code
-				crn
+				course_id
+				description
+				locations_summary
+				requirements
+				skills
+				evaluation_narratives {
+					comment
+				}
 			}
 		}
-		course_id
-		description
-		locations_summary
-		requirements
-		skills
-		evaluation_narratives {
-			comment
-		}
-	}
+		`,
+			variables: {
+				keyword,
+				course_keyword
+			}
+		})
+	};
+	const res = await fetch('https://api.coursetable.com/ferry/v1/graphql?=', options);
+	const response = await res.json() as SearchResponse;
+	return response
 }
-`,
-		variables: {
-			keyword,
-			course_keyword
-		}
-	})
-});
 
 export const GET: RequestHandler = async ({url}: {url: URL}) => {
 	const keyword = url.searchParams.get('keyword') ?? '%';
 	const course_keyword = url.searchParams.get('course_keyword') ?? '%';
-	const res = await fetch('https://api.coursetable.com/ferry/v1/graphql?=', query({keyword, course_keyword}));
-	const response = await res.json() as SearchResponse;
-	console.log("🚀 ~ file: +server.ts ~ line 78 ~ constGET:RequestHandler= ~ response", response)
+	// From https://stackoverflow.com/a/58437909
+	const response = await searchResponse({keyword, course_keyword});
+	console.log('🚀 ~ file: +server.ts ~ line 78 ~ constGET:RequestHandler= ~ response', response);
 	return json(response);
 };
