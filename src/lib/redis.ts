98d8f6e5a85c608f81842bb6f1d5ed04881be0f3
Braden Wong <bmw02002turbo@gmail.com>
2022-10-25 19:40:36 -0400

    Feat: Update redis

--- src/lib/redis.ts
+++ src/lib/redis.ts
@@ -1,7 +1,9 @@
+import Redis from 'ioredis';
 import { env } from '$env/dynamic/private';
-import { createClient } from 'redis'
 
-const REDIS_URL=`redis://${env.REDISUSER}:${env.REDISPASSWORD}@${env.REDISHOST}:${ env.REDISPORT }`
+const REDIS_URL = `redis://${env.REDISUSER}:${env.REDISPASSWORD}@${env.REDISHOST}:${env.REDISPORT}`;
 
-export const redis = createClient({ url: REDIS_URL })
-await redis.connect()
+const redis = REDIS_URL ? new Redis(REDIS_URL) : new Redis();
+console.log("🚀 ~ file: redis.ts ~ line 7 ~ redis", redis)
+
+export default redis
