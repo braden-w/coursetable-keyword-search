e576d3e6a10fd919a3d58a072a99acb760677c14
Braden Wong <bmw02002turbo@gmail.com>
2022-10-24 21:16:42 -0400

    Feat: Query

--- src/lib/suggested queries/QueryButton.svelte
+++ src/lib/suggested queries/QueryButton.svelte
@@ -3,6 +3,7 @@
 	export let course_keyword: string = '';
 	export let areas_skills_keyword: string = '';
 	export let title: string;
+	export let runQuery = async () => {};
 </script>
 
 <a
@@ -11,6 +12,7 @@
 		course_keyword: course_keyword,
 		areas_skills_keyword: areas_skills_keyword
 	})}"
+	on:click={runQuery}
 >
 	<span
 		class="inline-flex items-center rounded-full bg-indigo-300 px-3 py-0.5 text-sm font-medium text-gray-800"
--- src/routes/search/+page.svelte
+++ src/routes/search/+page.svelte
@@ -131,7 +131,7 @@
 			</div>
 		</div>
 	{/if}
-	<QueriesRow />
+	<QueriesRow {runQuery}/>
 
 	{#if coursesSortedByCount.length !== 0}
 		<div class="overflow-hidden bg-white shadow sm:rounded-md">
