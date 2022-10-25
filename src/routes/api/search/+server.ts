e3378d0db68391711939e42be45696a5d68946ae
Braden Wong <bmw02002turbo@gmail.com>
2022-10-25 12:18:33 -0400

    Feat: Redis caching

--- src/routes/api/search/+server.ts
+++ src/routes/api/search/+server.ts
@@ -3,6 +3,7 @@ import type { RequestHandler } from './$types';
 import { json } from '@sveltejs/kit';
 import type { SearchResponse } from '$lib/types/SearchResponse';
 import { options } from './payload';
+import { redis } from '$lib/redis'
 
 export const queryCourseTable = async ({
 	keyword,
@@ -27,13 +28,26 @@ export const queryCourseTable = async ({
 	course_keyword = course_keyword.toLowerCase();
 	console.log('🚀 ~ file: +server.ts ~ line 29 ~ course_keyword', course_keyword);
 
-	const res = await fetch(
-		'https://api.coursetable.com/ferry/v1/graphql?=',
-		options({ keyword, course_keyword, areas_skills_keyword })
-	);
-	const response = (await res.json()) as SearchResponse;
-	console.log('🚀 ~ file: +server.ts ~ line 25 ~ response', response);
-	return response;
+	const DEFAULT_EXPIRATION = 60 * 60 * 24 * 7; // 1 week
+
+	const key = `/api/search?keyword=${keyword}&course_keyword=${course_keyword}&areas_skills_keyword=${areas_skills_keyword}`;
+
+	const reply = await redis.get(key);
+	if (reply) {
+		console.log('Cache Hit');
+		const parsedReply = JSON.parse(reply) as SearchResponse
+		console.log("🚀 ~ file: +server.ts ~ line 39 ~ parsedReply", parsedReply)
+		return parsedReply;
+	} else {
+		console.log('Cache Miss');
+			const res = await fetch(
+				'https://api.coursetable.com/ferry/v1/graphql?=',
+				options({ keyword, course_keyword, areas_skills_keyword })
+			);
+		const response = await res.json() as SearchResponse
+		redis.setEx(key, DEFAULT_EXPIRATION, JSON.stringify(response));
+		return response;
+	}
 };
 
 export const GET: RequestHandler = async ({ url }: { url: URL }) => {
@@ -43,11 +57,11 @@ export const GET: RequestHandler = async ({ url }: { url: URL }) => {
 	// From https://stackoverflow.com/a/58437909
 	try {
 		const response = await queryCourseTable({ keyword, course_keyword, areas_skills_keyword });
-		const daysBeforeRevalidateCache = 7;
+		// const daysBeforeRevalidateCache = 7;
 		// https://vercel.com/docs/concepts/functions/serverless-functions/edge-caching
-		const cacheControl = `max-age=0, s-maxage=${60 * 60 * 24 * daysBeforeRevalidateCache}`;
+		// const cacheControl = `max-age=0, s-maxage=${60 * 60 * 24 * daysBeforeRevalidateCache}`;
 		return json(response, {
-			headers: { 'cache-control': cacheControl, 'Cache-Control': cacheControl }
+			// headers: { 'cache-control': cacheControl, 'Cache-Control': cacheControl }
 		});
 	} catch (e) {
 		throw error(500, e as Error);
