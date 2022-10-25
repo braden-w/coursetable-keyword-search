9251405c945b006da66b63f33a211587a9cc639a
Braden Wong <bmw02002turbo@gmail.com>
2022-10-24 22:10:30 -0400

    Fix: QueriesRow

--- src/routes/search/+page.svelte
+++ src/routes/search/+page.svelte
@@ -6,12 +6,10 @@
 	import { AcademicCap, BookOpen, Funnel, MagnifyingGlass } from '@steeze-ui/heroicons';
 	import { Icon } from '@steeze-ui/svelte-icon';
 	import type { SearchResponse } from '$lib/types/SearchResponse';
-	import type { PageData } from './$types';
 	import { onMount } from 'svelte';
 	import { goto } from '$app/navigation';
 	import QueriesRow from '$lib/suggested queries/QueriesRow.svelte';
 
-	export let data: PageData;
 	let keyword = $page.url.searchParams.get('keyword') ?? '';
 	let courseKeyword = $page.url.searchParams.get('course_keyword') ?? '';
 	let areasSkillsKeyword = $page.url.searchParams.get('areas_skills_keyword') ?? '';
@@ -56,30 +54,7 @@
 		courses = data.data.computed_listing_info_aggregate.nodes;
 		loading = false;
 	};
-	// onMount(runQuery);
-
-	import QueryButton from '$lib/suggested queries/QueryButton.svelte';
-	const queries = [
-		{
-			title: '"Favorite" humanities courses',
-			keyword: 'favorite',
-			course_keyword: 'Hu'
-		},
-		{
-			title: '"Best" QR courses',
-			keyword: 'best',
-			areas_skills_keyword: 'QR'
-		},
-		{
-			title: '"Quintessential" history courses',
-			keyword: 'quintessential',
-			course_keyword: 'HIST'
-		},
-		{
-			title: '"Favorite" in general',
-			keyword: 'favorite'
-		}
-	];
+	onMount(runQuery);
 </script>
 
 <div class="mx-auto max-w-7xl py-6 px-4 sm:px-6 lg:px-8">
@@ -157,23 +132,8 @@
 		</div>
 	{/if}
 
-	<span class="mb-2 flex gap-2 overflow-x-auto rounded-md shadow-sm">
-		{#each queries as query}
-			<a
-				href="/search?{new URLSearchParams({
-					keyword: query.keyword ?? '',
-					course_keyword: query.course_keyword ?? '',
-					areas_skills_keyword: query.areas_skills_keyword ?? ''
-				})}"
-			>
-				<span
-					class="inline-flex items-center rounded-full bg-indigo-300 px-3 py-0.5 text-sm font-medium text-gray-800"
-				>
-					{query.title}
-				</span>
-			</a>
-		{/each}
-	</span>
+	<QueriesRow />
+
 
 	{#if coursesSortedByCount.length !== 0}
 		<div class="overflow-hidden bg-white shadow sm:rounded-md">
