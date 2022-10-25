3afccfa6c1e84901cae433b43e30391bce06906d
Braden Wong <bmw02002turbo@gmail.com>
2022-10-25 19:50:47 -0400

    Feat: Use virtual list

--- src/routes/search/+page.svelte
+++ src/routes/search/+page.svelte
@@ -161,12 +161,12 @@
 	{#if coursesSortedByCount.length !== 0}
 		<div class="overflow-hidden bg-white shadow sm:rounded-md">
 			<ul class="divide-y divide-gray-200">
-				{#each coursesSortedByCount as course (course.listing_id)}
+				<!-- {#each coursesSortedByCount as course (course.listing_id)}
 					<ResultItem {course} />
-				{/each}
-				<!-- <VirtualList items={coursesSortedByCount} height="500px" let:item>
+				{/each} -->
+				<VirtualList items={coursesSortedByCount} height="500px" let:item>
 					<ResultItem course={item} />
-				</VirtualList> -->
+				</VirtualList>
 			</ul>
 		</div>
 	{:else}
