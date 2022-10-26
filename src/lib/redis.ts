60d8f2a34123ed047a67191131d385e94f4be89c
Braden Wong <bmw02002turbo@gmail.com>
2022-10-26 14:36:04 -0400

    Minor Tweaks to Query Row

--- src/lib/redis.ts
+++ src/lib/redis.ts
@@ -1,6 +1,6 @@
 import Redis from 'ioredis';
 import { env } from '$env/dynamic/private';
 
-const redis = new Redis(env.REDIS_URL as string)
+const redis = new Redis(env.REDIS_URL as string);
 
 export default redis;
