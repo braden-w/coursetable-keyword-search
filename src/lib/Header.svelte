52134a04deddb2e972d4631b90900e2341a7b4e9
Braden Wong <bmw02002turbo@gmail.com>
2022-10-26 11:45:07 -0400

    Padding

--- src/lib/Header.svelte
+++ src/lib/Header.svelte
@@ -41,7 +41,7 @@
 
 		<a
 			href="/about"
-			class="ml-6 inline-flex items-center rounded-md border border-transparent bg-indigo-600 px-4 py-4 text-sm font-medium text-white shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
+			class="ml-6 inline-flex items-center rounded-md border border-transparent bg-indigo-600 px-4 py-3 text-sm font-medium text-white shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
 		>
 			About
 		</a>
