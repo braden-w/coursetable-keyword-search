90b77f1b7d1a5edeb5d4e3c0b3380df33d71591a
Braden Wong <bmw02002turbo@gmail.com>
2022-10-25 22:17:56 -0400

    Logs

--- src/lib/redis.ts
+++ src/lib/redis.ts
@@ -4,6 +4,5 @@ import { env } from '$env/dynamic/private';
 const REDIS_URL = `redis://${env.REDISUSER}:${env.REDISPASSWORD}@${env.REDISHOST}:${env.REDISPORT}`;
 
 const redis = REDIS_URL ? new Redis(REDIS_URL) : new Redis();
-console.log('🚀 ~ file: redis.ts ~ line 7 ~ redis', redis);
 
 export default redis;
