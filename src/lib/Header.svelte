67018e40d28a0380e5093416fec5dc53d44db4fe
Braden Wong <bmw02002turbo@gmail.com>
2022-10-26 11:34:47 -0400

    Feat: About Button

--- src/lib/Header.svelte
+++ src/lib/Header.svelte
@@ -39,12 +39,14 @@
 					{/each}
 				</a>
 			</div>
-			<a
-				href="/about"
-				class="ml-6 inline-flex items-center rounded-md border border-transparent bg-indigo-600 px-4 py-2 text-sm font-medium text-white shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
-			>
-				About
-			</a>
+			<div class="flex items-center px-2 lg:px-0">
+				<a
+					href="/about"
+					class="ml-6 inline-flex items-center rounded-md border border-transparent bg-indigo-600 px-4 py-2 text-sm font-medium text-white shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
+				>
+					About
+				</a>
+			</div>
 		</div>
 	</div>
 </Disclosure>
