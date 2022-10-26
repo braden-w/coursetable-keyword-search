1f06b9ab20015e8b0df1877aa48e287f93ea7eec
Braden Wong <bmw02002turbo@gmail.com>
2022-10-25 20:05:10 -0400

    Feat: Use semester same course id

--- src/routes/search/+page.ts
+++ src/routes/search/+page.ts
@@ -2,8 +2,8 @@ import type { PageLoad } from './$types';
 
 export const load: PageLoad = async ({ fetch }) => {
 	// Get the file from static/same_course_id of this season
-	const response = await fetch('/same_course_id of this season/202301.json');
+	const response = await fetch('/semester_same_course_id/202301.json');
 	const data = await response.json() as number[];
 	console.log("🚀 ~ file: +page.ts ~ line 7 ~ constload:PageLoad= ~ data", data)
-	return data;
+	return { semester_same_course_id : data };
 };
