4c2c38ab8dcc9722b8e7e59e2476bc8a418c3ea1
Braden Wong <bmw02002turbo@gmail.com>
2022-10-27 00:55:13 -0400

    Feat: Trimmed graphQL query to fetch a lot less

--- src/routes/api/search/graphql.ts
+++ src/routes/api/search/graphql.ts
@@ -1,3 +1,43 @@
+// All fields:
+// 			all_course_codes
+// 			areas
+// 			average_gut_rating
+// 			average_professor
+// 			average_rating
+// 			average_workload
+// 			average_rating_same_professors
+// 			average_workload_same_professors
+// 			classnotes
+// 			course_code
+// 			credits
+// 			description
+// 			enrolled
+// 			extra_info
+// 			final_exam
+// 			flag_info
+// 			fysem
+// 			last_enrollment
+// 			last_enrollment_same_professors
+// 			listing_id
+// 			locations_summary
+// 			professor_ids
+// 			professor_names
+// 			regnotes
+// 			requirements
+// 			rp_attr
+// 			same_course_id
+// 			same_course_and_profs_id
+// 			last_offered_course_id
+// 			school
+// 			season_code
+// 			section
+// 			skills
+// 			subject
+// 			syllabus_url
+// 			times_by_day
+// 			times_summary
+// 			title
+
 const queryWithoutAreasSkillsKeyword = `query searchCoursesByKeyword(
 	$keyword: String!
 	$course_keyword: String!
@@ -17,41 +57,11 @@ const queryWithoutAreasSkillsKeyword = `query searchCoursesByKeyword(
 		nodes {
 			all_course_codes
 			areas
-			average_gut_rating
-			average_professor
-			average_rating
-			average_workload
-			average_rating_same_professors
-			average_workload_same_professors
-			classnotes
-			course_code
 			credits
 			description
-			enrolled
-			extra_info
-			final_exam
-			flag_info
-			fysem
-			last_enrollment
-			last_enrollment_same_professors
-			listing_id
-			locations_summary
-			professor_ids
-			professor_names
-			regnotes
-			requirements
-			rp_attr
 			same_course_id
-			same_course_and_profs_id
-			last_offered_course_id
-			school
 			season_code
-			section
 			skills
-			subject
-			syllabus_url
-			times_by_day
-			times_summary
 			title
 			course {
 				evaluation_narratives_aggregate_filtered: evaluation_narratives_aggregate(
@@ -100,41 +110,11 @@ const query = `query searchCoursesByKeyword(
 		nodes {
 			all_course_codes
 			areas
-			average_gut_rating
-			average_professor
-			average_rating
-			average_workload
-			average_rating_same_professors
-			average_workload_same_professors
-			classnotes
-			course_code
 			credits
 			description
-			enrolled
-			extra_info
-			final_exam
-			flag_info
-			fysem
-			last_enrollment
-			last_enrollment_same_professors
-			listing_id
-			locations_summary
-			professor_ids
-			professor_names
-			regnotes
-			requirements
-			rp_attr
 			same_course_id
-			same_course_and_profs_id
-			last_offered_course_id
-			school
 			season_code
-			section
 			skills
-			subject
-			syllabus_url
-			times_by_day
-			times_summary
 			title
 			course {
 				evaluation_narratives_aggregate_filtered: evaluation_narratives_aggregate(
