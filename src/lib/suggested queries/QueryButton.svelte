e91bd34037436eceada182d92852975e65df0fe8
Braden Wong <bmw02002turbo@gmail.com>
2022-10-27 23:53:47 -0400

    Refactor: Params

--- src/lib/suggested queries/QueryButton.svelte
+++ src/lib/suggested queries/QueryButton.svelte
@@ -1,26 +1,21 @@
 <script lang="ts">
 	import { goto } from '$app/navigation';
-	import type { PremadeQuery } from '$lib/types/Query';
+	import type { Params, PremadeQuery } from '$lib/types/Query';
 	import { createEventDispatcher } from 'svelte';
 
 	export let premadeQuery: PremadeQuery;
 	let { keyword, course_keyword, areas_skills_keyword, title } = premadeQuery;
-	const dispatch = createEventDispatcher<{
-		click: PremadeQuery;
-	}>();
+
+	const dispatch = createEventDispatcher<{ click: Params }>();
+
 	const onClick = () => {
-		dispatch('click', {
+		const params = {
 			keyword,
 			course_keyword,
 			areas_skills_keyword
-		});
-		goto(
-			`/search?${new URLSearchParams({
-				keyword: keyword ?? '',
-				course_keyword: course_keyword ?? '',
-				areas_skills_keyword: areas_skills_keyword ?? ''
-			})}`
-		);
+		};
+		dispatch('click', params);
+		goto(`/search?${new URLSearchParams(params)}`);
 	};
 </script>
 
