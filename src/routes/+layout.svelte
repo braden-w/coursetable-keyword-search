19c671bf4c4cd48b71a8c7c917ff6b5a598cabe5
Braden Wong <bmw02002turbo@gmail.com>
2022-10-28 01:00:11 -0400

    Feat: Analytics

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,9 +1,20 @@
 <script lang="ts">
 	import Footer from '$lib/Footer.svelte';
 	import Header from '$lib/Header.svelte';
-	import { inject } from '@vercel/analytics';
 	import '../app.postcss';
-	inject();
+	import { webVitals } from '$lib/vitals';
+	import { page } from '$app/stores';
+	import { browser } from '$app/environment';
+
+	let analyticsId = import.meta.env.VERCEL_ANALYTICS_ID;
+
+	$: if (browser && analyticsId) {
+		webVitals({
+			path: $page.url.pathname,
+			params: $page.params,
+			analyticsId
+		});
+	}
 </script>
 
 <!-- TODO: Update Head -->
