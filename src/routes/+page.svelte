2ebc369b77ea4fa9c062718177e8c40b2f7a1354
Braden Wong <bmw02002turbo@gmail.com>
2022-10-27 23:56:57 -0400

    Refactor home page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,20 +3,22 @@
 	import QueriesRow from '$lib/suggested queries/QueriesRow.svelte';
 	import { AcademicCap, BookOpen, Funnel, MagnifyingGlass } from '@steeze-ui/heroicons';
 	import { Icon } from '@steeze-ui/svelte-icon';
-	let keyword: string;
-	let course_keyword: string;
-	let areas_skills_keyword: string;
+
+	let keyword: string = '';
+	let course_keyword: string = '';
+	let areas_skills_keyword: string = '';
+	$: params = {
+		keyword,
+		course_keyword,
+		areas_skills_keyword
+	};
+
+	let showFilters = false;
+
 	const onKeydown = (e: KeyboardEvent) => {
 		if (e.key !== 'Enter') return;
-		goto(
-			`/search?${new URLSearchParams({
-				keyword: keyword ?? '',
-				course_keyword: course_keyword ?? '',
-				areas_skills_keyword: areas_skills_keyword ?? ''
-			})}`
-		);
+		goto(`/search?${new URLSearchParams(params)}`);
 	};
-	let showFilters = false;
 </script>
 
 <div class="relative">
