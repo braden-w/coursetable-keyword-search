ab19c0dc00ecd778ee10a0b82bd0c2b15266d2b1
Braden Wong <bmw02002turbo@gmail.com>
2022-10-29 11:57:20 -0400

    Feat: Animation and TM

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -23,10 +23,7 @@
 
 <div class="relative">
 	<div class="mx-auto max-w-7xl sm:px-6 lg:px-8">
-		<div class="relative shadow-xl sm:overflow-hidden sm:rounded-2xl">
-			<div class="absolute inset-0">
-				<div class="absolute inset-0 bg-base-200" />
-			</div>
+		<div class="relative sm:overflow-hidden sm:rounded-2xl">
 			<div class="relative px-4 py-16 sm:px-6 sm:py-24 lg:py-32 lg:px-16">
 				<h1 class="mb-2 text-4xl font-extrabold tracking-tight sm:text-5xl lg:text-6xl">
 					Search CourseTable reviews by keyword.
