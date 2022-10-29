f099ecbea09d2865c7f3879a6f25533b232215fb
Braden Wong <bmw02002turbo@gmail.com>
2022-10-29 16:51:43 -0400

    Feat: Navbar buttons now blend in

--- src/lib/Header.svelte
+++ src/lib/Header.svelte
@@ -42,14 +42,14 @@
 				<ThemeChooser></ThemeChooser>
 				<a
 					href="/about"
-					class="rounded-md px-3 py-2 text-sm font-medium text-gray-300 hover:bg-gray-700 hover:text-white"
+					class="btn btn-ghost gap-1 normal-case"
 				>
 					About
 				</a>
 
 				<a
 					href="/search"
-					class="rounded-md px-3 py-2 text-sm font-medium text-gray-300 hover:bg-gray-700 hover:text-white"
+					class="btn btn-ghost gap-1 normal-case"
 				>
 					Search
 				</a>
