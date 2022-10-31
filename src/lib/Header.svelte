b75953d972919a07e0fa8223c5eabd49f1e78902
Braden Wong <bmw02002turbo@gmail.com>
2022-10-31 15:10:01 -0400

    Feat: New Tab Github Page

--- src/lib/Header.svelte
+++ src/lib/Header.svelte
@@ -42,7 +42,7 @@
 		<div class="block">
 			<div class="flex space-x-4">
 				<!-- GitHub Icon -->
-				<a href="https://github.com/braden-w/coursetable-keyword-search" class="btn-ghost btn normal-case">
+				<a href="https://github.com/braden-w/coursetable-keyword-search" class="btn-ghost btn normal-case" target="_blank" rel="noreferrer">
 					<Icon src={GithubLogo} class="h-6 w-6" aria-hidden="true" />
 				</a>
 				<ThemeChooser />
