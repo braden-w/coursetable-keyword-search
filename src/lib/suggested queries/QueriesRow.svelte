88188ed32946560e4e0f392bf0c2580a7bd3f09c
Braden Wong <bmw02002turbo@gmail.com>
2022-11-02 17:12:34 -0400

    Feat: Show Left Arrow

--- src/lib/suggested queries/QueriesRow.svelte
+++ src/lib/suggested queries/QueriesRow.svelte
@@ -4,6 +4,7 @@
 	import { Icon } from '@steeze-ui/svelte-icon';
 	import QueryButton from './QueryButton.svelte';
 	let scroll: HTMLDivElement;
+	let showLeftArrow = false
 	function scrollLeft() {
 		// Scroll element left smoothly
 		scroll.scrollBy({ left: -scroll.offsetWidth, behavior: 'smooth' });
@@ -11,13 +12,16 @@
 	function scrollRight() {
 		// Scroll element right smoothly
 		scroll.scrollBy({ left: scroll.offsetWidth, behavior: 'smooth' });
+		showLeftArrow = true;
 	}
 </script>
 
 <div bind:this={scroll} class="flex snap-x gap-1.5 overflow-x-auto md:snap-none md:gap-2">
-	<button on:click={scrollLeft} class="absolute left-9 mt-1">
-		<Icon src={ArrowLeft} class="h-4 w-4" />
-	</button>
+	{#if showLeftArrow}
+		<button on:click={scrollLeft} class="absolute left-9 mt-1">
+			<Icon src={ArrowLeft} class="h-4 w-4" />
+		</button>
+	{/if}
 	{#each premadeQueries as premadeQuery (premadeQuery.title)}
 		<div class="snap-start">
 			<QueryButton {premadeQuery} on:click />
@@ -29,8 +33,8 @@
 </div>
 
 <style>
-/* Hide the scrollbar */
-::-webkit-scrollbar {
-	display: none;
-}
+	/* Hide the scrollbar */
+	::-webkit-scrollbar {
+		display: none;
+	}
 </style>
