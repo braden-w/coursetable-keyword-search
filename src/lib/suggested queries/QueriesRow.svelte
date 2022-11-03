45486e849cd05f9382e4043ad9ab92b1ae895e07
Braden Wong <bmw02002turbo@gmail.com>
2022-11-02 21:10:59 -0400

    Feat: Relative positioning for arrows

--- src/lib/suggested queries/QueriesRow.svelte
+++ src/lib/suggested queries/QueriesRow.svelte
@@ -4,7 +4,7 @@
 	import { Icon } from '@steeze-ui/svelte-icon';
 	import QueryButton from './QueryButton.svelte';
 	let scroll: HTMLDivElement;
-	let showLeftArrow = false
+	let showLeftArrow = false;
 	function scrollLeft() {
 		// Scroll element left smoothly
 		scroll.scrollBy({ left: -scroll.offsetWidth, behavior: 'smooth' });
@@ -16,18 +16,24 @@
 	}
 </script>
 
-<div bind:this={scroll} class="flex snap-x gap-1.5 overflow-x-auto md:snap-none md:gap-2">
+<div class="relative">
 	{#if showLeftArrow}
-		<button on:click={scrollLeft} class="absolute left-7 mt-1">
+		<button on:click={scrollLeft} class="absolute inset-y-0 left-0 -ml-6">
 			<Icon src={ArrowLeft} class="h-4 w-4" />
 		</button>
 	{/if}
-	{#each premadeQueries as premadeQuery (premadeQuery.title)}
-		<div class="snap-start">
-			<QueryButton {premadeQuery} on:click />
-		</div>
-	{/each}
-	<button on:click={scrollRight} class="absolute right-7 mt-1">
+	<div bind:this={scroll} class="flex snap-x gap-1.5 overflow-x-auto py-1 md:snap-none md:gap-2">
+		{#each premadeQueries as premadeQuery (premadeQuery.title)}
+			<div class="snap-start">
+				<QueryButton {premadeQuery} on:click />
+			</div>
+		{/each}
+	</div>
+	<button
+		on:click={scrollRight}
+		class="absolute inset-y-0 right-0 z-10 -mr-6"
+		class:animate-bounce={!showLeftArrow}
+	>
 		<Icon src={ArrowRight} class="h-4 w-4" />
 	</button>
 </div>
