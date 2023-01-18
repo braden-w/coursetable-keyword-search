58fd4e92951fde6c209089a4cdcef75eb2894684
Braden Wong <bmw02002turbo@gmail.com>
2023-01-18 15:40:59 -0500

    Feat: Use Catalog QPI

--- src/routes/search/+page.ts
+++ src/routes/search/+page.ts
@@ -1,9 +1,8 @@
-import { SEASON_ID } from '$lib/constants';
 import type { PageLoad } from './$types';
 
 export const load: PageLoad = async ({ fetch }) => {
 	// Get the file from static/same_course_id of this season
-	const response = await fetch(`/seasons/courseIds/${SEASON_ID}.json`);
+	const response = await fetch('/api/catalog');
 	const seasonCourseIds = (await response.json()) as { [key: number]: string };
 	return { seasonCourseIds };
 };
