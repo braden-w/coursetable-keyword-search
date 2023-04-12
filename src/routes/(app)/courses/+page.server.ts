8dfbfe75e2d1cd69955420196c194b6c9b7d3e11
Braden Wong <bmw02002turbo@gmail.com>
2023-04-12 01:43:47 -0400

    chore: page.server.ts

---
+++ src/routes/(app)/courses/+page.server.ts
@@ -0,0 +1,7 @@
+import {trpc} from "$lib/trpc";
+
+export const load = async (event) => {
+	return {
+		courses: trpc.allCourses.ssr(event)
+	};
+};
--- src/routes/(app)/courses/+page.ts
+++
@@ -1,7 +0,0 @@
-import { db } from '$lib/db';
-
-export const load = async () => {
-	return {
-		courses: db.selectFrom('Course').selectAll().execute()
-	};
-};
