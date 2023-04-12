c3e56df0ece70192f7144b7436718a9526fe9443
Braden Wong <bmw02002turbo@gmail.com>
2023-04-11 22:05:16 -0400

    feat: node server for cron job

---
+++ scheduled-tasks/index.js
@@ -0,0 +1,70 @@
+import dotenv from 'dotenv';
+import fetch from 'node-fetch';
+dotenv.config();
+
+const options = {
+	method: 'POST',
+	headers: {
+		'Content-Type': 'application/json',
+		Cookie: process.env.PUBLIC_COURSETABLE_COOKIE
+	},
+	body: JSON.stringify({
+		query: `query {
+	computed_listing_info(distinct_on: same_course_id) {
+		all_course_codes
+		areas
+		average_gut_rating
+		average_professor
+		average_rating
+		average_workload
+		average_rating_same_professors
+		average_workload_same_professors
+		classnotes
+		course_code
+		credits
+		crn
+		description
+		enrolled
+		extra_info
+		final_exam
+		flag_info
+		fysem
+		last_enrollment
+		last_enrollment_same_professors
+		listing_id
+		locations_summary
+		number
+		professor_ids
+		professor_names
+		regnotes
+		requirements
+		rp_attr
+		same_course_id
+		same_course_and_profs_id
+		last_offered_course_id
+		school
+		season_code
+		section
+		skills
+		subject
+		syllabus_url
+		times_by_day
+		times_summary
+		title
+	}
+	}`,
+		variables: {}
+	})
+};
+
+const makeRequest = async () => {
+	try {
+		const response = await fetch('https://api.coursetable.com/ferry/v1/graphql', options);
+		const data = await response.json();
+		console.log(data);
+	} catch (err) {
+		console.error(err);
+	}
+};
+
+makeRequest();
