95732165dc3d7645204ed8336e349fb12e7b0a92
Braden Wong <bmw02002turbo@gmail.com>
2023-04-12 22:18:48 -0400

    fix: Courses instead of Course

--- src/lib/server/routes/_app.ts
+++ src/lib/server/routes/_app.ts
@@ -4,21 +4,21 @@ import { publicProcedure, router } from '../trpc';
 
 export const appRouter = router({
 	allCourses: publicProcedure.query(async () => {
-		const courses = await db.selectFrom('Course').selectAll().execute();
+		const courses = await db.selectFrom('Courses').selectAll().execute();
 		return courses;
 	}),
 	allCoursesMatchingSeason: publicProcedure
 		.input(z.object({ seasonCode: z.string() }))
 		.query(async ({ input: { seasonCode } }) => {
 			const courses = await db
-				.selectFrom('Course')
+				.selectFrom('Courses')
 				.selectAll()
 				.where('season_code', '=', seasonCode)
 				.execute();
 			return courses;
 		}),
 	allCoursesLight: publicProcedure.query(async () => {
-		const courses = await db.selectFrom('Course').select(['all_course_codes']).execute();
+		const courses = await db.selectFrom('Courses').select(['all_course_codes']).execute();
 		return courses;
 	})
 });
