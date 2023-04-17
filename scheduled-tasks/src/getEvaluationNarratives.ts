4359329b948c18e0bd0499ff0adaee2c8b842dbf
Braden Wong <bmw02002turbo@gmail.com>
2023-04-17 02:05:11 -0400

    feat: getEvaluationNarratives follows getCourses

--- scheduled-tasks/src/getEvaluationNarratives.ts
+++ scheduled-tasks/src/getEvaluationNarratives.ts
@@ -1,23 +1,49 @@
 import { fetchCourseTable } from './fetchCourseTable.js';
 
-async function getEvaluationNarratives() {
-	const query = `
-	query {
-		evaluation_narratives {
-			id
-			course_id
-			comment
-			comment_compound
+const evaluationNarrativesCount = `query EvaluationNarrativesCount {
+	evaluation_narratives_aggregate {
+		aggregate {
+			count
 		}
 	}
-	`;
-	const variables = {};
+}`;
 
+const evaluationNarratives = `query EvaluationNarratives ($limit: Int, $offset: Int) {
+	evaluation_narratives (limit: $limit, offset: $offset) {
+		id
+		course_id
+		comment
+		comment_compound
+	}
+}`;
+
+export async function getEvaluationNarrativesCount() {
 	try {
 		const {
-			data: { evaluation_narratives }
-		} = await fetchCourseTable(query, variables);
-		return evaluation_narratives;
+			data: {
+				evaluation_narratives_aggregate: {
+					aggregate: { count }
+				}
+			}
+		} = await fetchCourseTable(evaluationNarrativesCount);
+		return count;
+	} catch (err) {
+		console.error(err);
+	}
+}
+
+export async function getEvaluationNarratives() {
+	const count = await getEvaluationNarrativesCount();
+	const parallelRequests = 4;
+	const limit = Math.ceil(count / parallelRequests);
+
+	try {
+		const requests = Array.from({ length: parallelRequests }, (_, i) => i * limit).map((offset) =>
+			fetchCourseTable(getEvaluationNarratives, { limit, offset })
+		);
+		const results = await Promise.all(requests);
+		const evaluationNarratives = results.flatMap((result) => result.data.evaluation_narratives);
+		return evaluationNarratives;
 	} catch (err) {
 		console.error(err);
 	}
