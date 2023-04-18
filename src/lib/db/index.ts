5b32c6117add7ae6eddd0cd06bc00f98d487d0d3
Braden Wong <bmw02002turbo@gmail.com>
2023-04-17 20:42:26 -0400

    fix: kysely generation

--- src/lib/db/index.ts
+++ src/lib/db/index.ts
@@ -1,13 +1,19 @@
-import { DATABASE_URL } from '$env/static/private';
-import { Kysely, PostgresDialect, sql, type RawBuilder } from 'kysely';
-import { Pool } from 'pg';
+import { Kysely, type RawBuilder, sql } from 'kysely';
+import { PostgresJSDialect } from 'kysely-postgres-js';
 import type { DB } from './schema';
+import postgres from 'postgres';
+import { DATABASE_HOST, DATABASE_USERNAME, DATABASE_PASSWORD } from '$env/static/private';
 
 export const db = new Kysely<DB>({
-	dialect: new PostgresDialect({
-		pool: new Pool({
-			connectionString: DATABASE_URL
-		})
+	dialect: new PostgresJSDialect({
+		options: {
+			database: 'postgres',
+			host: DATABASE_HOST,
+			port: 5432,
+			user: DATABASE_USERNAME,
+			password: DATABASE_PASSWORD
+		},
+		postgres
 	})
 });
 
