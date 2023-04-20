8f8445105a5286abb902024dcfc1178f482d492c
Braden Wong <bmw02002turbo@gmail.com>
2023-04-19 21:17:28 -0400

    chroe: update getCourses query

--- scheduled-tasks/src/courses/getCourses.ts
+++ scheduled-tasks/src/courses/getCourses.ts
@@ -10,7 +10,6 @@ const coursesCount = `query CoursesCount {
 
 const coursesSchemaMatchingApiStaticCatalog = `query CoursesSchemaMatchingApiStaticCatalog ($limit: Int, $offset: Int) {
 	computed_listing_info (limit: $limit, offset: $offset, distinct_on: course_id) {
-		course_id
 		all_course_codes
 		areas
 		average_gut_rating
@@ -21,34 +20,21 @@ const coursesSchemaMatchingApiStaticCatalog = `query CoursesSchemaMatchingApiSta
 		average_workload_same_professors
 		classnotes
 		course_code
+		course_id
 		credits
-		crn
 		description
-		enrolled
-		extra_info
 		final_exam
 		flag_info
-		fysem
-		last_enrollment
-		last_enrollment_same_professors
-		listing_id
 		locations_summary
 		number
-		professor_ids
 		professor_names
 		regnotes
 		requirements
-		rp_attr
 		same_course_id
-		same_course_and_profs_id
-		last_offered_course_id
 		school
 		season_code
 		section
 		skills
-		subject
-		syllabus_url
-		times_by_day
 		times_summary
 		title
 	}
