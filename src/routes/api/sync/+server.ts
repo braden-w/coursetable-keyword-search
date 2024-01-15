28431a366fa01e51c1ec207a5e82603a4d570f22
Braden Wong <13159333+braden-w@users.noreply.github.com>
2024-01-15 16:21:28 -0500

    refactor: fetchGraphQl

--- src/routes/api/sync/+server.ts
+++ src/routes/api/sync/+server.ts
@@ -175,16 +175,17 @@ const queries = {
 };
 
 export const GET = async () => {
-	const options = {
-		method: 'POST',
-		headers: {
-			cookie: COURSETABLE_COOKIE,
-			'Content-Type': 'application/json'
-		},
-		body: '{"query":"query AllCourseEvaluations {\n  courses {\n    course_id\n    title\n    evaluation_narratives {\n      comment_compound\n    }\n  }\n}\n","operationName":"AllCourseEvaluations"}'
-	};
+	const fetchGraphQL = (query: string) =>
+		fetch('https://api.coursetable.com/ferry/v1/graphql', {
+			method: 'POST',
+			headers: {
+				cookie: COURSETABLE_COOKIE,
+				'Content-Type': 'application/json'
+			},
+			body: JSON.stringify({ query })
+		});
 
-	const res = await fetch('https://api.coursetable.com/ferry/v1/graphql', options);
+	const res = await fetchGraphQL(queries['Seasons']);
 	if (!res.ok) {
 		return error(res.status, res.statusText);
 	}
