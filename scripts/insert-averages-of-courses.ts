e4e8099b2ec3a2c23b0f2147939c1008e56b34f0
Braden Wong <13159333+braden-w@users.noreply.github.com>
2024-01-16 11:13:25 -0500

    refactor: average instead of avg

--- scripts/insert-averages-of-courses.ts
+++ scripts/insert-averages-of-courses.ts
@@ -4,30 +4,30 @@ import { courses, evaluation_narratives } from '../src/lib/server/schema';
 
 export async function insertAveragesOfCourses() {
 	try {
-		await db.run(sql`WITH avg_comments AS (
+		await db.run(sql`WITH average_comments AS (
 			SELECT
 				${evaluation_narratives.course_id},
-				AVG(${evaluation_narratives.comment_neg}) as avg_comment_neg,
-				COUNT(${evaluation_narratives.comment_neg}) as avg_comment_neg_n,
-				AVG(${evaluation_narratives.comment_neu}) as avg_comment_neu,
-				COUNT(${evaluation_narratives.comment_neu}) as avg_comment_neu_n,
-				AVG(${evaluation_narratives.comment_pos}) as avg_comment_pos,
-				COUNT(${evaluation_narratives.comment_pos}) as avg_comment_pos_n,
-				AVG(${evaluation_narratives.comment_compound}) as avg_comment_compound,
-				COUNT(${evaluation_narratives.comment_compound}) as avg_comment_compound_n
+				AVG(${evaluation_narratives.comment_neg}) as average_comment_neg,
+				COUNT(${evaluation_narratives.comment_neg}) as average_comment_neg_n,
+				AVG(${evaluation_narratives.comment_neu}) as average_comment_neu,
+				COUNT(${evaluation_narratives.comment_neu}) as average_comment_neu_n,
+				AVG(${evaluation_narratives.comment_pos}) as average_comment_pos,
+				COUNT(${evaluation_narratives.comment_pos}) as average_comment_pos_n,
+				AVG(${evaluation_narratives.comment_compound}) as average_comment_compound,
+				COUNT(${evaluation_narratives.comment_compound}) as average_comment_compound_n
 			FROM ${evaluation_narratives}
 			GROUP BY ${evaluation_narratives.course_id}
 		)
 		UPDATE ${courses}
 			SET
-				average_comment_neg = (SELECT avg_comment_neg FROM avg_comments WHERE ${courses.course_id} = avg_comments.course_id),
-				average_comment_neg_n = (SELECT avg_comment_neg_n FROM avg_comments WHERE ${courses.course_id} = avg_comments.course_id),
-				average_comment_neu = (SELECT avg_comment_neu FROM avg_comments WHERE ${courses.course_id} = avg_comments.course_id),
-				average_comment_neu_n = (SELECT avg_comment_neu_n FROM avg_comments WHERE ${courses.course_id} = avg_comments.course_id),
-				average_comment_pos = (SELECT avg_comment_pos FROM avg_comments WHERE ${courses.course_id} = avg_comments.course_id),
-				average_comment_pos_n = (SELECT avg_comment_pos_n FROM avg_comments WHERE ${courses.course_id} = avg_comments.course_id),
-				average_comment_compound = (SELECT avg_comment_compound FROM avg_comments WHERE ${courses.course_id} = avg_comments.course_id);
-				average_comment_compound_n = (SELECT avg_comment_compound_n FROM avg_comments WHERE ${courses.course_id} = avg_comments.course_id);
+				average_comment_neg = (SELECT average_comment_neg FROM average_comments WHERE ${courses.course_id} = average_comments.course_id),
+				average_comment_neg_n = (SELECT average_comment_neg_n FROM average_comments WHERE ${courses.course_id} = average_comments.course_id),
+				average_comment_neu = (SELECT average_comment_neu FROM average_comments WHERE ${courses.course_id} = average_comments.course_id),
+				average_comment_neu_n = (SELECT average_comment_neu_n FROM average_comments WHERE ${courses.course_id} = average_comments.course_id),
+				average_comment_pos = (SELECT average_comment_pos FROM average_comments WHERE ${courses.course_id} = average_comments.course_id),
+				average_comment_pos_n = (SELECT average_comment_pos_n FROM average_comments WHERE ${courses.course_id} = average_comments.course_id),
+				average_comment_compound = (SELECT average_comment_compound FROM average_comments WHERE ${courses.course_id} = average_comments.course_id);
+				average_comment_compound_n = (SELECT average_comment_compound_n FROM average_comments WHERE ${courses.course_id} = average_comments.course_id);
 		`);
 	} catch (e) {
 		console.error(e);
