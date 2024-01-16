08149fba13dfbfb95933ddd1567201e136773f12
Braden Wong <13159333+braden-w@users.noreply.github.com>
2024-01-16 11:12:44 -0500

    feat: script now also updates average comment n

--- scripts/insert-averages-of-courses.ts
+++ scripts/insert-averages-of-courses.ts
@@ -8,18 +8,26 @@ export async function insertAveragesOfCourses() {
 			SELECT
 				${evaluation_narratives.course_id},
 				AVG(${evaluation_narratives.comment_neg}) as avg_comment_neg,
+				COUNT(${evaluation_narratives.comment_neg}) as avg_comment_neg_n,
 				AVG(${evaluation_narratives.comment_neu}) as avg_comment_neu,
+				COUNT(${evaluation_narratives.comment_neu}) as avg_comment_neu_n,
 				AVG(${evaluation_narratives.comment_pos}) as avg_comment_pos,
-				AVG(${evaluation_narratives.comment_compound}) as avg_comment_compound
+				COUNT(${evaluation_narratives.comment_pos}) as avg_comment_pos_n,
+				AVG(${evaluation_narratives.comment_compound}) as avg_comment_compound,
+				COUNT(${evaluation_narratives.comment_compound}) as avg_comment_compound_n
 			FROM ${evaluation_narratives}
 			GROUP BY ${evaluation_narratives.course_id}
 		)
 		UPDATE ${courses}
 			SET
 				average_comment_neg = (SELECT avg_comment_neg FROM avg_comments WHERE ${courses.course_id} = avg_comments.course_id),
+				average_comment_neg_n = (SELECT avg_comment_neg_n FROM avg_comments WHERE ${courses.course_id} = avg_comments.course_id),
 				average_comment_neu = (SELECT avg_comment_neu FROM avg_comments WHERE ${courses.course_id} = avg_comments.course_id),
+				average_comment_neu_n = (SELECT avg_comment_neu_n FROM avg_comments WHERE ${courses.course_id} = avg_comments.course_id),
 				average_comment_pos = (SELECT avg_comment_pos FROM avg_comments WHERE ${courses.course_id} = avg_comments.course_id),
+				average_comment_pos_n = (SELECT avg_comment_pos_n FROM avg_comments WHERE ${courses.course_id} = avg_comments.course_id),
 				average_comment_compound = (SELECT avg_comment_compound FROM avg_comments WHERE ${courses.course_id} = avg_comments.course_id);
+				average_comment_compound_n = (SELECT avg_comment_compound_n FROM avg_comments WHERE ${courses.course_id} = avg_comments.course_id);
 		`);
 	} catch (e) {
 		console.error(e);
