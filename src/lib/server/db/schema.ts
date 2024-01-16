84d6164ca9e0968060a4e6b8cb907ee9a39db443
Braden Wong <13159333+braden-w@users.noreply.github.com>
2024-01-15 20:50:24 -0500

    feat: snake_case all columns

--- src/lib/server/db/schema.ts
+++ src/lib/server/db/schema.ts
@@ -12,7 +12,7 @@ import {
 } from 'drizzle-orm/sqlite-core';
 
 export const seasons = sqliteTable('seasons', {
-	seasonCode: text('season_code').primaryKey(),
+	season_code: text('season_code').primaryKey(),
 	term: text('term'),
 	year: integer('year'),
 });
@@ -22,60 +22,60 @@ export const insertSeasonSchema = createInsertSchema(seasons);
 export const courses = sqliteTable(
 	'courses',
 	{
-		courseId: integer('course_id').primaryKey(),
-		seasonCode: text('season_code')
+		course_id: integer('course_id').primaryKey(),
+		season_code: text('season_code')
 			.notNull()
-			.references(() => seasons.seasonCode),
+			.references(() => seasons.season_code),
 		title: text('title'),
-		shortTitle: text('short_title'),
+		short_title: text('short_title'),
 		description: text('description'),
 		requirements: text('requirements'),
-		locationsSummary: text('locations_summary'),
-		timesLongSummary: text('times_long_summary'),
-		timesSummary: text('times_summary'),
-		timesByDay: text('times_by_day'),
+		locations_summary: text('locations_summary'),
+		times_long_summary: text('times_long_summary'),
+		times_summary: text('times_summary'),
+		times_by_day: text('times_by_day'),
 		skills: text('skills'),
 		areas: text('areas'),
 		credits: real('credits'),
-		syllabusUrl: text('syllabus_url'),
-		courseHomeUrl: text('course_home_url'),
+		syllabus_url: text('syllabus_url'),
+		course_home_url: text('course_home_url'),
 		regnotes: text('regnotes'),
-		extraInfo: text('extra_info'),
-		rpAttr: text('rp_attr'),
+		extra_info: text('extra_info'),
+		rp_attr: text('rp_attr'),
 		classnotes: text('classnotes'),
-		finalExam: text('final_exam'),
+		final_exam: text('final_exam'),
 		fysem: numeric('fysem'),
 		sysem: numeric('sysem'),
 		colsem: numeric('colsem'),
-		averageRating: real('average_rating'),
-		averageRatingN: integer('average_rating_n'),
-		averageWorkload: real('average_workload'),
-		averageWorkloadN: integer('average_workload_n'),
-		averageRatingSameProfessors: real('average_rating_same_professors'),
-		averageRatingSameProfessorsN: integer('average_rating_same_professors_n'),
-		averageWorkloadSameProfessors: real('average_workload_same_professors'),
-		averageWorkloadSameProfessorsN: integer('average_workload_same_professors_n'),
-		lastOfferedCourseId: integer('last_offered_course_id'),
-		sameCourseId: integer('same_course_id').notNull(),
-		sameCourseAndProfsId: integer('same_course_and_profs_id').notNull(),
-		lastEnrollmentCourseId: integer('last_enrollment_course_id'),
-		lastEnrollment: integer('last_enrollment'),
-		lastEnrollmentSeasonCode: text('last_enrollment_season_code').references(
-			() => seasons.seasonCode,
+		average_rating: real('average_rating'),
+		average_rating_n: integer('average_rating_n'),
+		average_workload: real('average_workload'),
+		average_workload_n: integer('average_workload_n'),
+		average_rating_same_professors: real('average_rating_same_professors'),
+		average_rating_same_professors_n: integer('average_rating_same_professors_n'),
+		average_workload_same_professors: real('average_workload_same_professors'),
+		average_workload_same_professors_n: integer('average_workload_same_professors_n'),
+		last_offered_course_id: integer('last_offered_course_id'),
+		same_course_id: integer('same_course_id').notNull(),
+		same_course_and_profs_id: integer('same_course_and_profs_id').notNull(),
+		last_enrollment_course_id: integer('last_enrollment_course_id'),
+		last_enrollment: integer('last_enrollment'),
+		last_enrollment_season_code: text('last_enrollment_season_code').references(
+			() => seasons.season_code,
 		),
 	},
 	(table) => {
 		return {
-			coursesLastOfferedCourseIdCoursesCourseIdFk: foreignKey({
-				columns: [table.lastOfferedCourseId],
-				foreignColumns: [table.courseId],
-				name: 'courses_last_offered_course_id_courses_course_id_fk',
-			}),
-			coursesLastEnrollmentCourseIdCoursesCourseIdFk: foreignKey({
-				columns: [table.lastEnrollmentCourseId],
-				foreignColumns: [table.courseId],
+			courses_last_enrollment_course_id_courses_course_id_fk: foreignKey(() => ({
+				columns: [table.last_enrollment_course_id],
+				foreignColumns: [table.course_id],
 				name: 'courses_last_enrollment_course_id_courses_course_id_fk',
-			}),
+			})),
+			courses_last_offered_course_id_courses_course_id_fk: foreignKey(() => ({
+				columns: [table.last_offered_course_id],
+				foreignColumns: [table.course_id],
+				name: 'courses_last_offered_course_id_courses_course_id_fk',
+			})),
 		};
 	},
 );
@@ -85,28 +85,28 @@ export const insertCourseSchema = createInsertSchema(courses);
 export const listings = sqliteTable(
 	'listings',
 	{
-		listingId: integer('listing_id').primaryKey(),
-		courseId: integer('course_id')
+		listing_id: integer('listing_id').primaryKey(),
+		course_id: integer('course_id')
 			.notNull()
-			.references(() => courses.courseId),
+			.references(() => courses.course_id),
 		school: text('school'),
 		subject: text('subject').notNull(),
 		number: text('number').notNull(),
-		courseCode: text('course_code'),
+		course_code: text('course_code'),
 		section: text('section').notNull(),
-		seasonCode: text('season_code')
+		season_code: text('season_code')
 			.notNull()
-			.references(() => seasons.seasonCode),
+			.references(() => seasons.season_code),
 		crn: integer('crn').notNull(),
 	},
 	(table) => {
 		return {
-			idxSeasonCodeCrnUnique: uniqueIndex('idx_season_code_crn_unique').on(
-				table.seasonCode,
+			idx_season_code_crn_unique: uniqueIndex('idx_season_code_crn_unique').on(
+				table.season_code,
 				table.crn,
 			),
-			idxSeasonCourseSectionUnique: index('idx_season_course_section_unique').on(
-				table.seasonCode,
+			idx_seasonCourseSection_unique: index('idx_season_course_section_unique').on(
+				table.season_code,
 				table.subject,
 				table.number,
 				table.section,
@@ -118,66 +118,66 @@ export const listings = sqliteTable(
 export const insertListingSchema = createInsertSchema(listings);
 
 export const discussions = sqliteTable('discussions', {
-	discussionId: integer('discussion_id').primaryKey(),
+	discussion_id: integer('discussion_id').primaryKey(),
 	subject: text('subject').notNull(),
 	number: text('number').notNull(),
 	info: text('info'),
-	locationsSummary: text('locations_summary'),
-	timesLongSummary: text('times_long_summary'),
-	timesSummary: text('times_summary'),
-	timesByDay: text('times_by_day'),
+	locations_summary: text('locations_summary'),
+	times_long_summary: text('times_long_summary'),
+	times_summary: text('times_summary'),
+	times_by_day: text('times_by_day'),
 });
 
 export const insertDiscussionSchema = createInsertSchema(discussions);
 
 export const flags = sqliteTable('flags', {
-	flagId: integer('flag_id').primaryKey(),
-	flagText: text('flag_text').notNull(),
+	flag_id: integer('flag_id').primaryKey(),
+	flag_text: text('flag_text').notNull(),
 });
 
 export const insertFlagSchema = createInsertSchema(flags);
 
 export const demandStatistics = sqliteTable('demand_statistics', {
-	courseId: integer('course_id')
+	course_id: integer('course_id')
 		.primaryKey()
-		.references(() => courses.courseId),
-	latestDemand: integer('latest_demand'),
-	latestDemandDate: text('latest_demand_date'),
+		.references(() => courses.course_id),
+	latest_demand: integer('latest_demand'),
+	latest_demand_date: text('latest_demand_date'),
 	demand: text('demand'),
 });
 
 export const insertDemandStatisticsSchema = createInsertSchema(demandStatistics);
 
 export const professors = sqliteTable('professors', {
-	professorId: integer('professor_id').primaryKey(),
+	professor_id: integer('professor_id').primaryKey(),
 	name: text('name').notNull(),
 	email: text('email'),
-	averageRating: real('average_rating'),
-	averageRatingN: integer('average_rating_n'),
+	average_rating: real('average_rating'),
+	average_rating_n: integer('average_rating_n'),
 });
 
 export const insertProfessorSchema = createInsertSchema(professors);
 
 export const evaluationStatistics = sqliteTable('evaluation_statistics', {
-	courseId: integer('course_id')
+	course_id: integer('course_id')
 		.primaryKey()
-		.references(() => courses.courseId),
+		.references(() => courses.course_id),
 	enrollment: integer('enrollment'),
 	enrolled: integer('enrolled'),
 	responses: integer('responses'),
 	declined: integer('declined'),
-	noResponse: integer('no_response'),
+	no_response: integer('no_response'),
 	extras: text('extras'),
-	avgRating: real('avg_rating'),
-	avgWorkload: real('avg_workload'),
+	avg_rating: real('avg_rating'),
+	avg_workload: real('avg_workload'),
 });
 
 export const insertEvaluationStatisticsSchema = createInsertSchema(evaluationStatistics);
 
 export const evaluationQuestions = sqliteTable('evaluation_questions', {
-	questionCode: text('question_code').primaryKey(),
-	isNarrative: numeric('is_narrative'),
-	questionText: text('question_text'),
+	question_code: text('question_code').primaryKey(),
+	is_narrative: numeric('is_narrative'),
+	question_text: text('question_text'),
 	options: text('options'),
 	tag: text('tag'),
 });
@@ -186,29 +186,29 @@ export const insertEvaluationQuestionSchema = createInsertSchema(evaluationQuest
 
 export const evaluationNarratives = sqliteTable('evaluation_narratives', {
 	id: integer('id').primaryKey(),
-	courseId: integer('course_id')
+	course_id: integer('course_id')
 		.notNull()
-		.references(() => courses.courseId),
-	questionCode: text('question_code')
+		.references(() => courses.course_id),
+	question_code: text('question_code')
 		.notNull()
-		.references(() => evaluationQuestions.questionCode),
-	comment: text('COMMENT'),
-	commentNeg: real('comment_neg'),
-	commentNeu: real('comment_neu'),
-	commentPos: real('comment_pos'),
-	commentCompound: real('comment_compound'),
+		.references(() => evaluationQuestions.question_code),
+	comment: text('comment'),
+	comment_neg: real('comment_neg'),
+	comment_neu: real('comment_neu'),
+	comment_pos: real('comment_pos'),
+	comment_compound: real('comment_compound'),
 });
 
 export const insertEvaluationNarrativeSchema = createInsertSchema(evaluationNarratives);
 
 export const evaluationRatings = sqliteTable('evaluation_ratings', {
 	id: integer('id').primaryKey(),
-	courseId: integer('course_id')
+	course_id: integer('course_id')
 		.notNull()
-		.references(() => courses.courseId),
-	questionCode: text('question_code')
+		.references(() => courses.course_id),
+	question_code: text('question_code')
 		.notNull()
-		.references(() => evaluationQuestions.questionCode),
+		.references(() => evaluationQuestions.question_code),
 	rating: text('rating'),
 });
 
@@ -217,13 +217,13 @@ export const insertEvaluationRatingSchema = createInsertSchema(evaluationRatings
 export const courseProfessors = sqliteTable(
 	'course_professors',
 	{
-		courseId: integer('course_id').references(() => courses.courseId),
-		professorId: integer('professor_id').references(() => professors.professorId),
+		course_id: integer('course_id').references(() => courses.course_id),
+		professor_id: integer('professor_id').references(() => professors.professor_id),
 	},
 	(table) => {
 		return {
 			pk0: primaryKey({
-				columns: [table.courseId, table.professorId],
+				columns: [table.course_id, table.professor_id],
 				name: 'course_professors_course_id_professor_id_pk',
 			}),
 		};
@@ -235,44 +235,42 @@ export const insertCourseProfessorSchema = createInsertSchema(courseProfessors);
 export const courseDiscussions = sqliteTable(
 	'course_discussions',
 	{
-		courseId: integer('course_id').references(() => courses.courseId),
-		discussionId: integer('discussion_id').references(() => discussions.discussionId),
+		course_id: integer('course_id').references(() => courses.course_id),
+		discussion_id: integer('discussion_id').references(() => discussions.discussion_id),
 	},
 	(table) => {
 		return {
 			pk0: primaryKey({
-				columns: [table.courseId, table.discussionId],
+				columns: [table.course_id, table.discussion_id],
 				name: 'course_discussions_course_id_discussion_id_pk',
 			}),
 		};
 	},
 );
-
 export const insertCourseDiscussionSchema = createInsertSchema(courseDiscussions);
 
 export const courseFlags = sqliteTable(
 	'course_flags',
 	{
-		courseId: integer('course_id').references(() => courses.courseId),
-		flagId: integer('flag_id').references(() => flags.flagId),
+		course_id: integer('course_id').references(() => courses.course_id),
+		flag_id: integer('flag_id').references(() => flags.flag_id),
 	},
 	(table) => {
 		return {
 			pk0: primaryKey({
-				columns: [table.courseId, table.flagId],
+				columns: [table.course_id, table.flag_id],
 				name: 'course_flags_course_id_flag_id_pk',
 			}),
 		};
 	},
 );
-
 export const insertCourseFlagSchema = createInsertSchema(courseFlags);
 
 export const fasttextSimilars = sqliteTable(
 	'fasttext_similars',
 	{
-		source: integer('source').references(() => courses.courseId),
-		target: integer('target').references(() => courses.courseId),
+		source: integer('source').references(() => courses.course_id),
+		target: integer('target').references(() => courses.course_id),
 		rank: integer('rank'),
 	},
 	(table) => {
@@ -290,8 +288,8 @@ export const insertFasttextSimilarSchema = createInsertSchema(fasttextSimilars);
 export const tfidfSimilars = sqliteTable(
 	'tfidf_similars',
 	{
-		source: integer('source').references(() => courses.courseId),
-		target: integer('target').references(() => courses.courseId),
+		source: integer('source').references(() => courses.course_id),
+		target: integer('target').references(() => courses.course_id),
 		rank: integer('rank'),
 	},
 	(table) => {
