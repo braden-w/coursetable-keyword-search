e1a0788f362f1d33f65e08fef5fa9cdb3b918203
Braden Wong <13159333+braden-w@users.noreply.github.com>
2024-01-16 10:41:34 -0500

    feat: update and set

--- insert-averages-of-courses.ts
+++ insert-averages-of-courses.ts
@@ -4,27 +4,22 @@ import { courses, evaluation_narratives } from './src/lib/server/schema';
 
 export async function insertAveragesOfCourses() {
 	try {
-		await db.run(sql`UPDATE ${courses}
-			SET ${sql.raw(`average_comment_neg`)} = (
-					SELECT AVG(${evaluation_narratives.comment_neg})
-					FROM ${evaluation_narratives}
-					WHERE ${courses.course_id} = ${evaluation_narratives.course_id}
-			),
-			${sql.raw(`average_comment_neu`)} = (
-					SELECT AVG(${evaluation_narratives.comment_neu})
-					FROM ${evaluation_narratives}
-					WHERE ${courses.course_id} = ${evaluation_narratives.course_id}
-			),
-			${sql.raw(`average_comment_pos`)} = (
-					SELECT AVG(${evaluation_narratives.comment_pos})
-					FROM ${evaluation_narratives}
-					WHERE ${courses.course_id} = ${evaluation_narratives.course_id}
-			),
-			${sql.raw(`average_comment_compound`)} = (
-					SELECT AVG(${evaluation_narratives.comment_compound})
-					FROM ${evaluation_narratives}
-					WHERE ${courses.course_id} = ${evaluation_narratives.course_id}
-			);
+		await db.run(sql`WITH avg_comments AS (
+			SELECT
+        course_id,
+        AVG(comment_neg) AS avg_comment_neg,
+        AVG(comment_neu) AS avg_comment_neu,
+        AVG(comment_pos) AS avg_comment_pos,
+        AVG(comment_compound) AS avg_comment_compound
+			FROM evaluation_narratives
+			GROUP BY course_id
+		)
+		UPDATE courses
+			SET
+				average_comment_neg = (SELECT avg_comment_neg FROM avg_comments WHERE courses.course_id = avg_comments.course_id),
+				average_comment_neu = (SELECT avg_comment_neu FROM avg_comments WHERE courses.course_id = avg_comments.course_id),
+				average_comment_pos = (SELECT avg_comment_pos FROM avg_comments WHERE courses.course_id = avg_comments.course_id),
+				average_comment_compound = (SELECT avg_comment_compound FROM avg_comments WHERE courses.course_id = avg_comments.course_id);
 		`);
 	} catch (e) {
 		console.error(e);
