adfd18ad657180d49499f898d75746205c32b9a0
Braden Wong <13159333+braden-w@users.noreply.github.com>
2024-01-16 11:31:51 -0500

    feat: readonly tokens

--- src/lib/server/db.ts
+++ src/lib/server/db.ts
@@ -1,12 +1,12 @@
 import { drizzle } from 'drizzle-orm/libsql';
 import { createClient } from '@libsql/client';
-import { TURSO_SYNC_URL, TURSO_TOKEN } from '$env/static/private';
+import { TURSO_SYNC_URL, TURSO_READONLY_TOKEN } from '$env/static/private';
 import * as schema from './schema';
 
 export const client = createClient({
-	// url: TURSO_SYNC_URL as string,
-	// authToken: TURSO_TOKEN as string,
-	url: 'file:./courses.sqlite',
+	url: TURSO_SYNC_URL as string,
+	authToken: TURSO_READONLY_TOKEN as string,
+	// url: 'file:./courses.sqlite',
 });
 
 export const db = drizzle(client, { logger: true, schema });
