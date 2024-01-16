746666a36c425bc85dcb7e83a279e1c794b7d97b
Braden Wong <13159333+braden-w@users.noreply.github.com>
2024-01-16 01:45:47 -0500

    feat: simplify classes of button

--- src/lib/suggested_queries/QueriesRow.svelte
+++ src/lib/suggested_queries/QueriesRow.svelte
@@ -4,6 +4,7 @@
 	import ArrowRight from '~icons/heroicons/arrow-right';
 	import QueryButton from './QueryButton.svelte';
 	import { Button } from '$lib/components/ui/button';
+	import { cn } from '$lib/utils';
 	let scroll: HTMLDivElement;
 	let showLeftArrow = false;
 	function scrollLeft() {
@@ -19,12 +20,7 @@
 
 <div class="relative">
 	{#if showLeftArrow}
-		<Button
-			variant="ghost"
-			size="icon"
-			class="absolute inset-y-0 left-0 -ml-6"
-			on:click={scrollLeft}
-		>
+		<Button variant="ghost" size="icon" class="absolute left-0 -ml-6" on:click={scrollLeft}>
 			<ArrowLeft class="h-4 w-4" />
 		</Button>
 	{/if}
@@ -38,9 +34,8 @@
 	<Button
 		variant="ghost"
 		size="icon"
-		class="absolute inset-y-0 right-0 z-10 -mr-6"
+		class={cn('absolute right-0 -mr-6', !showLeftArrow && 'animate-bounce')}
 		on:click={scrollRight}
-		class:animate-bounce={!showLeftArrow}
 	>
 		<ArrowRight class="h-4 w-4" />
 	</Button>
