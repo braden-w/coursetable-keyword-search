a14937c7524a234885ded4794b144b69cfda9f4c
Braden Wong <13159333+braden-w@users.noreply.github.com>
2024-01-16 01:43:21 -0500

    feat: simplify boilerplate

--- src/routes/(app)/+page.svelte
+++ src/routes/(app)/+page.svelte
@@ -34,7 +34,7 @@
 	});
 </script>
 
-<div class="container max-w-6xl flex flex-col gap-4">
+<div class="container max-w-6xl flex flex-col gap-4 h-full justify-center">
 	<h1 class="scroll-m-20 text-4xl font-extrabold tracking-tight sm:text-5xl lg:text-6xl">
 		Search Yale Course reviews by
 		<span bind:this={typewriter} class="font-extrabold text-primary-content" />
@@ -48,84 +48,83 @@
 			<strong>CourseTable</strong>
 		</a>. Try now:
 	</h2>
-	<div class="">
-		<div class="flex w-full">
-			<label for="search" class="sr-only">Search</label>
-			<div class="relative flex-1">
+	<div class="flex w-full">
+		<label for="search" class="sr-only">Search</label>
+		<div class="relative flex-1">
+			<div class="pointer-events-none absolute inset-y-0 left-0 flex items-center pl-3">
+				<MagnifyingGlass class="h-5 w-5 text-gray-400" aria-hidden="true" />
+			</div>
+			<input
+				id="search"
+				name="search"
+				class="block w-full rounded-md border border-gray-300 bg-white py-2 pl-10 pr-3 text-sm text-gray-900 placeholder-gray-500 focus:border-primary focus:placeholder-gray-400 focus:outline-none focus:ring-1 focus:ring-primary sm:text-sm"
+				placeholder="Search by review keyword...(use % to match anything)"
+				type="search"
+				bind:value={keyword}
+				on:keydown={onKeydown}
+			/>
+		</div>
+		<label for="filters" class="sr-only">Show filters</label>
+		<span class="sr-only">Loading...</span>
+		<span class="relative inline-flex">
+			<button
+				id="filters"
+				type="button"
+				name="filters"
+				class="flex rounded-md border border-gray-300 bg-white px-3.5 py-2 shadow-sm hover:bg-gray-50 focus:outline-none focus:ring-1 focus:ring-primary focus:ring-offset-2"
+				on:click={() => (showFilters = !showFilters)}
+			>
+				<Funnel class="h-5 w-5 text-gray-400" aria-hidden="true" />
+				<span class="sr-only">Filter</span>
+			</button>
+			{#if !showFilters}
+				<span class="absolute right-0 top-0 -mr-1 -mt-1 flex h-2 w-2">
+					<span
+						class="absolute inline-flex h-full w-full animate-ping rounded-full bg-secondary opacity-75"
+					/>
+					<span class="relative inline-flex h-2 w-2 rounded-full bg-secondary" />
+				</span>
+			{/if}
+		</span>
+	</div>
+	{#if showFilters}
+		<div class="my-2 w-full">
+			<label for="search" class="sr-only">Course Filter</label>
+			<div class="relative">
 				<div class="pointer-events-none absolute inset-y-0 left-0 flex items-center pl-3">
-					<MagnifyingGlass class="h-5 w-5 text-gray-400" aria-hidden="true" />
+					<BookOpen class="h-5 w-5 text-gray-400" aria-hidden="true" />
 				</div>
 				<input
 					id="search"
 					name="search"
 					class="block w-full rounded-md border border-gray-300 bg-white py-2 pl-10 pr-3 text-sm text-gray-900 placeholder-gray-500 focus:border-primary focus:placeholder-gray-400 focus:outline-none focus:ring-1 focus:ring-primary sm:text-sm"
-					placeholder="Search by review keyword...(use % to match anything)"
+					placeholder="Filter by course code ...(ECON, PLSC, HIST, etc.)"
 					type="search"
-					bind:value={keyword}
+					bind:value={course_keyword}
 					on:keydown={onKeydown}
 				/>
 			</div>
-			<label for="filters" class="sr-only">Show filters</label>
-			<span class="sr-only">Loading...</span>
-			<span class="relative inline-flex">
-				<button
-					id="filters"
-					type="button"
-					name="filters"
-					class="flex rounded-md border border-gray-300 bg-white px-3.5 py-2 shadow-sm hover:bg-gray-50 focus:outline-none focus:ring-1 focus:ring-primary focus:ring-offset-2"
-					on:click={() => (showFilters = !showFilters)}
-				>
-					<Funnel class="h-5 w-5 text-gray-400" aria-hidden="true" />
-					<span class="sr-only">Filter</span>
-				</button>
-				{#if !showFilters}
-					<span class="absolute right-0 top-0 -mr-1 -mt-1 flex h-2 w-2">
-						<span
-							class="absolute inline-flex h-full w-full animate-ping rounded-full bg-secondary opacity-75"
-						/>
-						<span class="relative inline-flex h-2 w-2 rounded-full bg-secondary" />
-					</span>
-				{/if}
-			</span>
 		</div>
-		{#if showFilters}
-			<div class="my-2 w-full">
-				<label for="search" class="sr-only">Course Filter</label>
-				<div class="relative">
-					<div class="pointer-events-none absolute inset-y-0 left-0 flex items-center pl-3">
-						<BookOpen class="h-5 w-5 text-gray-400" aria-hidden="true" />
-					</div>
-					<input
-						id="search"
-						name="search"
-						class="block w-full rounded-md border border-gray-300 bg-white py-2 pl-10 pr-3 text-sm text-gray-900 placeholder-gray-500 focus:border-primary focus:placeholder-gray-400 focus:outline-none focus:ring-1 focus:ring-primary sm:text-sm"
-						placeholder="Filter by course code ...(ECON, PLSC, HIST, etc.)"
-						type="search"
-						bind:value={course_keyword}
-						on:keydown={onKeydown}
-					/>
-				</div>
-			</div>
 
-			<div class="my-2 w-full">
-				<label for="search" class="sr-only">Areas Filter</label>
-				<div class="relative">
-					<div class="pointer-events-none absolute inset-y-0 left-0 flex items-center pl-3">
-						<AcademicCap class="h-5 w-5 text-gray-400" aria-hidden="true" />
-					</div>
-					<input
-						id="search"
-						name="search"
-						class="block w-full rounded-md border border-gray-300 bg-white py-2 pl-10 pr-3 text-sm text-gray-900 placeholder-gray-500 focus:border-primary focus:placeholder-gray-400 focus:outline-none focus:ring-1 focus:ring-primary sm:text-sm"
-						placeholder="Filter by areas ...(Hu, Qr, So, etc., case sensitive)"
-						type="search"
-						bind:value={areas_skills_keyword}
-						on:keydown={onKeydown}
-					/>
+		<div class="my-2 w-full">
+			<label for="search" class="sr-only">Areas Filter</label>
+			<div class="relative">
+				<div class="pointer-events-none absolute inset-y-0 left-0 flex items-center pl-3">
+					<AcademicCap class="h-5 w-5 text-gray-400" aria-hidden="true" />
 				</div>
+				<input
+					id="search"
+					name="search"
+					class="block w-full rounded-md border border-gray-300 bg-white py-2 pl-10 pr-3 text-sm text-gray-900 placeholder-gray-500 focus:border-primary focus:placeholder-gray-400 focus:outline-none focus:ring-1 focus:ring-primary sm:text-sm"
+					placeholder="Filter by areas ...(Hu, Qr, So, etc., case sensitive)"
+					type="search"
+					bind:value={areas_skills_keyword}
+					on:keydown={onKeydown}
+				/>
 			</div>
-		{/if}
-	</div>
+		</div>
+	{/if}
+
 	<QueriesRow />
 	<!-- Add screenshot with rounded corners -->
 	<div class="relative">
