31af7e56531c42f16d17b021078a547db2cae54b
Braden Wong <13159333+braden-w@users.noreply.github.com>
2024-01-15 20:10:28 -0500

    refactor: query

--- src/routes/api/sync/+server.ts
+++ src/routes/api/sync/+server.ts
@@ -6,220 +6,220 @@ const TABLES = [
 	{
 		name: 'seasons',
 		query: `query {
-		seasons {
-			season_code
-			term
-			year
-		}
-	}`
+			seasons {
+				season_code
+				term
+				year
+			}
+		}`
 	},
 	{
 		name: 'courses',
 		query: `query {
-		courses {
-			course_id
-			season_code
-			title
-			short_title
-			description
-			requirements
-			locations_summary
-			times_long_summary
-			times_summary
-			times_by_day
-			skills
-			areas
-			credits
-			syllabus_url
-			course_home_url
-			regnotes
-			extra_info
-			rp_attr
-			classnotes
-			final_exam
-			fysem
-			sysem
-			colsem
-			average_rating
-			average_rating_n
-			average_workload
-			average_workload_n
-			average_rating_same_professors
-			average_rating_same_professors_n
-			average_workload_same_professors
-			average_workload_same_professors_n
-			last_offered_course_id
-			same_course_id
-			same_course_and_profs_id
-			last_enrollment_course_id
-			last_enrollment
-			last_enrollment_season_code
-		}
-	}`
+			courses {
+				course_id
+				season_code
+				title
+				short_title
+				description
+				requirements
+				locations_summary
+				times_long_summary
+				times_summary
+				times_by_day
+				skills
+				areas
+				credits
+				syllabus_url
+				course_home_url
+				regnotes
+				extra_info
+				rp_attr
+				classnotes
+				final_exam
+				fysem
+				sysem
+				colsem
+				average_rating
+				average_rating_n
+				average_workload
+				average_workload_n
+				average_rating_same_professors
+				average_rating_same_professors_n
+				average_workload_same_professors
+				average_workload_same_professors_n
+				last_offered_course_id
+				same_course_id
+				same_course_and_profs_id
+				last_enrollment_course_id
+				last_enrollment
+				last_enrollment_season_code
+			}
+		}`
 	},
 	{
 		name: 'listings',
 		query: `query {
-		listings {
-			listing_id
-			course_id
-			school
-			subject
-			number
-			course_code
-			section
-			season_code
-			crn
-		}
-	}`
+			listings {
+				listing_id
+				course_id
+				school
+				subject
+				number
+				course_code
+				section
+				season_code
+				crn
+			}
+		}`
 	},
 	{
 		name: 'discussions',
 		query: `query {
-		discussions {
-			discussion_id
-			subject
-			number
-			info
-			locations_summary
-			times_long_summary
-			times_summary
-			times_by_day
-		}
-	}`
+			discussions {
+				discussion_id
+				subject
+				number
+				info
+				locations_summary
+				times_long_summary
+				times_summary
+				times_by_day
+			}
+		}`
 	},
 	{
 		name: 'flags',
 		query: `query {
-		flags {
-			flag_id
-			flag_text
-		}
-	}`
+			flags {
+				flag_id
+				flag_text
+			}
+		}`
 	},
 	{
 		name: 'demand_statistics',
 		query: `query {
-		demand_statistics {
-			course_id
-			latest_demand
-			latest_demand_date
-			demand
-		}
-	}`
+			demand_statistics {
+				course_id
+				latest_demand
+				latest_demand_date
+				demand
+			}
+		}`
 	},
 	{
 		name: 'professors',
 		query: `query {
-		professors {
-			professor_id
-			name
-			email
-			average_rating
-			average_rating_n
-		}
-	}`
+			professors {
+				professor_id
+				name
+				email
+				average_rating
+				average_rating_n
+			}
+		}`
 	},
 	{
 		name: 'evaluation_statistics',
 		query: `query {
-		evaluation_statistics {
-			course_id
-			enrollment
-			enrolled
-			responses
-			declined
-			no_response
-			extras
-			avg_rating
-			avg_workload
-		}
-	}`
+			evaluation_statistics {
+				course_id
+				enrollment
+				enrolled
+				responses
+				declined
+				no_response
+				extras
+				avg_rating
+				avg_workload
+			}
+		}`
 	},
 	{
 		name: 'evaluation_questions',
 		query: `query {
-		evaluation_questions {
-			question_code
-			is_narrative
-			question_text
-			options
-			tag
-		}
-	}`
+			evaluation_questions {
+				question_code
+				is_narrative
+				question_text
+				options
+				tag
+			}
+		}`
 	},
 	{
 		name: 'evaluation_narratives',
 		query: `query {
-		evaluation_narratives {
-			id
-			course_id
-			question_code
-			comment
-			comment_neg
-			comment_neu
-			comment_pos
-			comment_compound
-		}
-	}`
+			evaluation_narratives {
+				id
+				course_id
+				question_code
+				comment
+				comment_neg
+				comment_neu
+				comment_pos
+				comment_compound
+			}
+		}`
 	},
 	{
 		name: 'evaluation_ratings',
 		query: `query {
-		evaluation_ratings {
-			id
-			course_id
-			question_code
-			rating
-		}
-	}`
+			evaluation_ratings {
+				id
+				course_id
+				question_code
+				rating
+			}
+		}`
 	},
 	{
 		name: 'course_professors',
 		query: `query {
-		course_professors {
-			course_id
-			professor_id
-		}
-	}`
+			course_professors {
+				course_id
+				professor_id
+			}
+		}`
 	},
 	{
 		name: 'course_discussions',
 		query: `query {
-		course_discussions {
-			course_id
-			discussion_id
-		}
-	}`
+			course_discussions {
+				course_id
+				discussion_id
+			}
+		}`
 	},
 	{
 		name: 'course_flags',
 		query: `query {
-		course_flags {
-			course_id
-			flag_id
-		}
-	}`
+			course_flags {
+				course_id
+				flag_id
+			}
+		}`
 	},
 	{
 		name: 'fasttext_similars',
 		query: `query {
-		fasttext_similars {
-			source
-			target
-			rank
-		}
-	}`
+			fasttext_similars {
+				source
+				target
+				rank
+			}
+		}`
 	},
 	{
 		name: 'tfidf_similars',
 		query: `query {
-		tfidf_similars {
-			source
-			target
-			rank
-		}
-	}`
+			tfidf_similars {
+				source
+				target
+				rank
+			}
+		}`
 	}
 ] as const;
 
