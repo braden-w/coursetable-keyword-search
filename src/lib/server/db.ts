740a9ca8fb8d81417611dfd0130e2d92dfd65984
Braden Wong <13159333+braden-w@users.noreply.github.com>
2024-01-16 11:16:40 -0500

    feat: add schema to drizzle

--- src/lib/server/db.ts
+++ src/lib/server/db.ts
@@ -1,6 +1,7 @@
 import { drizzle } from 'drizzle-orm/libsql';
 import { createClient } from '@libsql/client';
 import { TURSO_SYNC_URL, TURSO_TOKEN } from '$env/static/private';
+import * as schema from './schema';
 
 export const client = createClient({
 	// url: TURSO_SYNC_URL as string,
@@ -8,4 +9,4 @@ export const client = createClient({
 	url: 'file:./courses.sqlite',
 });
 
-export const db = drizzle(client, { logger: true });
+export const db = drizzle(client, { logger: true, schema });
