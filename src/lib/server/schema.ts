5c4c856a2cf4dc241571355f813c9c892f6f7996
Braden Wong <13159333+braden-w@users.noreply.github.com>
2024-01-16 11:20:01 -0500

    chore: imports

--- src/lib/server/schema.ts
+++ src/lib/server/schema.ts
@@ -1,13 +1,12 @@
 import { relations } from 'drizzle-orm';
 import {
-	foreignKey,
 	index,
 	integer,
 	primaryKey,
 	real,
 	sqliteTable,
 	text,
-	uniqueIndex,
+	uniqueIndex
 } from 'drizzle-orm/sqlite-core';
 import { createInsertSchema } from 'drizzle-zod';
 import { z } from 'zod';
