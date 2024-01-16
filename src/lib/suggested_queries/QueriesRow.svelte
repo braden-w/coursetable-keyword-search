de69b41339d20f205f712e91854870ae2d8c1240
Braden Wong <13159333+braden-w@users.noreply.github.com>
2024-01-16 01:44:56 -0500

    feat: Button and such!

--- src/lib/suggested_queries/QueriesRow.svelte
+++ src/lib/suggested_queries/QueriesRow.svelte
@@ -1,8 +1,9 @@
 <script lang="ts">
 	import { premadeQueries } from '$lib/suggested_queries/premadeQueries';
-	import ArrowLeft from '~icons/heroicons/arrow-left'
-	import ArrowRight from '~icons/heroicons/arrow-right'
+	import ArrowLeft from '~icons/heroicons/arrow-left';
+	import ArrowRight from '~icons/heroicons/arrow-right';
 	import QueryButton from './QueryButton.svelte';
+	import { Button } from '$lib/components/ui/button';
 	let scroll: HTMLDivElement;
 	let showLeftArrow = false;
 	function scrollLeft() {
@@ -18,9 +19,14 @@
 
 <div class="relative">
 	{#if showLeftArrow}
-		<button on:click={scrollLeft} class="absolute inset-y-0 left-0 -ml-6">
+		<Button
+			variant="ghost"
+			size="icon"
+			class="absolute inset-y-0 left-0 -ml-6"
+			on:click={scrollLeft}
+		>
 			<ArrowLeft class="h-4 w-4" />
-		</button>
+		</Button>
 	{/if}
 	<div bind:this={scroll} class="flex snap-x gap-1.5 overflow-x-auto py-1 md:snap-none md:gap-2">
 		{#each premadeQueries as premadeQuery (premadeQuery.title)}
@@ -29,13 +35,15 @@
 			</div>
 		{/each}
 	</div>
-	<button
-		on:click={scrollRight}
+	<Button
+		variant="ghost"
+		size="icon"
 		class="absolute inset-y-0 right-0 z-10 -mr-6"
+		on:click={scrollRight}
 		class:animate-bounce={!showLeftArrow}
 	>
 		<ArrowRight class="h-4 w-4" />
-	</button>
+	</Button>
 </div>
 
 <style>
