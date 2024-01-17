159535f30e1e7ce3b5dfbeee070c9e67c012080a
Braden Wong <13159333+braden-w@users.noreply.github.com>
2024-01-16 22:41:16 -0500

    feat: scaffold table

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -10,6 +10,52 @@
 	import Funnel from '~icons/heroicons/funnel';
 	import MagnifyingGlass from '~icons/heroicons/magnifying-glass';
 	import screenshotCropped from '$lib/assets/screenshot_cropped.jpg';
+	import * as Table from '$lib/components/ui/table';
+
+	const invoices = [
+		{
+			invoice: 'INV001',
+			paymentStatus: 'Paid',
+			totalAmount: '$250.00',
+			paymentMethod: 'Credit Card',
+		},
+		{
+			invoice: 'INV002',
+			paymentStatus: 'Pending',
+			totalAmount: '$150.00',
+			paymentMethod: 'PayPal',
+		},
+		{
+			invoice: 'INV003',
+			paymentStatus: 'Unpaid',
+			totalAmount: '$350.00',
+			paymentMethod: 'Bank Transfer',
+		},
+		{
+			invoice: 'INV004',
+			paymentStatus: 'Paid',
+			totalAmount: '$450.00',
+			paymentMethod: 'Credit Card',
+		},
+		{
+			invoice: 'INV005',
+			paymentStatus: 'Paid',
+			totalAmount: '$550.00',
+			paymentMethod: 'PayPal',
+		},
+		{
+			invoice: 'INV006',
+			paymentStatus: 'Pending',
+			totalAmount: '$200.00',
+			paymentMethod: 'Bank Transfer',
+		},
+		{
+			invoice: 'INV007',
+			paymentStatus: 'Unpaid',
+			totalAmount: '$300.00',
+			paymentMethod: 'Credit Card',
+		},
+	];
 
 	let typewriter: HTMLSpanElement;
 	onMount(() => {
@@ -95,6 +141,27 @@
 	</Collapsible.Root>
 	<QueriesRow />
 	<!-- Add screenshot with rounded corners -->
+	<Table.Root>
+		<Table.Caption>A list of your recent invoices.</Table.Caption>
+		<Table.Header>
+			<Table.Row>
+				<Table.Head class="w-[100px]">Invoice</Table.Head>
+				<Table.Head>Status</Table.Head>
+				<Table.Head>Method</Table.Head>
+				<Table.Head class="text-right">Amount</Table.Head>
+			</Table.Row>
+		</Table.Header>
+		<Table.Body>
+			{#each invoices as invoice, i (i)}
+				<Table.Row>
+					<Table.Cell class="font-medium">{invoice.invoice}</Table.Cell>
+					<Table.Cell>{invoice.paymentStatus}</Table.Cell>
+					<Table.Cell>{invoice.paymentMethod}</Table.Cell>
+					<Table.Cell class="text-right">{invoice.totalAmount}</Table.Cell>
+				</Table.Row>
+			{/each}
+		</Table.Body>
+	</Table.Root>
 	<div class="relative">
 		<img src={screenshotCropped} class="rounded-xl" alt="Screenshot" />
 		<div class="absolute inset-0 bg-gradient-to-t from-base-100" />
