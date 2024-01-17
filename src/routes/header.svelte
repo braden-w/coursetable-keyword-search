cd1ec694877486cd6f28005a30b01b255bf23c76
Braden Wong <13159333+braden-w@users.noreply.github.com>
2024-01-17 15:38:40 -0500

    feat: carousel

--- src/routes/header.svelte
+++ src/routes/header.svelte
@@ -1,8 +1,9 @@
 <script lang="ts">
 	import logo from '$lib/assets/logo.png';
 	import { Button } from '$lib/components/ui/button';
+	import * as Carousel from '$lib/components/ui/carousel/index';
+	import Autoplay from 'embla-carousel-autoplay';
 	import { onDestroy, onMount } from 'svelte';
-	import { fade } from 'svelte/transition';
 	import GithubLogo from '~icons/mdi/github';
 
 	const titles = [
@@ -29,17 +30,23 @@
 	class="sticky top-0 z-50 w-full border-b border-border/40 bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60"
 >
 	<div class="container flex h-14 max-w-screen-2xl items-center justify-between">
-		<a class="flex gap-4" href="/">
+		<a class="flex gap-4 items-center" href="/">
 			<img src={logo} alt="Logo" class="h-8 rounded-md" />
-			<button on:click={cycleTitles}>
-				{#each titles as title, index}
-					{#if titleIndex === index}
-						<span class="font-bold" transition:fade={{ duration: 800, easing: (t) => t * (2 - t) }}>
-							{@html title}
-						</span>
-					{/if}
-				{/each}
-			</button>
+			<Carousel.Root
+				class="w-full max-w-[160px]"
+				opts={{ loop: true, align: 'start' }}
+				plugins={[
+					Autoplay({
+						delay: 1000,
+					}),
+				]}
+			>
+				<Carousel.Content>
+					{#each titles as title}
+						<Carousel.Item><span class="font-bold">{@html title}</span></Carousel.Item>
+					{/each}
+				</Carousel.Content>
+			</Carousel.Root>
 		</a>
 		<div class="flex gap-2">
 			<Button
