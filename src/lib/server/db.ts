93a70e54a0e2ebe216a9f5c252711aa3bf4cdca3
Braden Wong <13159333+braden-w@users.noreply.github.com>
2024-01-16 22:37:14 -0500

    feat: for now, use turso dev mode

--- src/lib/server/db.ts
+++ src/lib/server/db.ts
@@ -1,11 +1,12 @@
-import { drizzle } from 'drizzle-orm/libsql';
+// import { TURSO_READONLY_TOKEN, TURSO_SYNC_URL } from '$env/static/private';
 import { createClient } from '@libsql/client';
-import { TURSO_SYNC_URL, TURSO_READONLY_TOKEN } from '$env/static/private';
+import { drizzle } from 'drizzle-orm/libsql';
 import * as schema from './schema';
 
 export const client = createClient({
-	url: TURSO_SYNC_URL as string,
-	authToken: TURSO_READONLY_TOKEN as string,
+	// url: TURSO_SYNC_URL as string,
+	// authToken: TURSO_READONLY_TOKEN as string,
+	url: 'http://127.0.0.1:8080',
 	// url: 'file:./courses.sqlite',
 });
 
