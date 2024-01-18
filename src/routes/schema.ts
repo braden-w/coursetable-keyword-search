ab1ac34cf36857689bd6380fdde9f9a5e705b407
Braden Wong <13159333+braden-w@users.noreply.github.com>
2024-01-18 15:06:01 -0500

    chore: move schema to search-config

--- src/routes/schema.ts
+++
@@ -1,8 +0,0 @@
-import { allCourseColumnNames } from '$lib/schema';
-import { z } from 'zod';
-
-export const orderByConfigSchema = z
-	.object({ column: z.enum(allCourseColumnNames), direction: z.enum(['asc', 'desc']) })
-	.array();
-
-export type OrderByConfig = z.infer<typeof orderByConfigSchema>;
