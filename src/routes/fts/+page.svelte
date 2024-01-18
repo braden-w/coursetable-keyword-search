ab0e0d6e9b464da5aae52ef40f85f4d5533c1cd6
Braden Wong <13159333+braden-w@users.noreply.github.com>
2024-01-18 14:41:38 -0500

    feat: headings

--- src/routes/fts/+page.svelte
+++ src/routes/fts/+page.svelte
@@ -51,6 +51,12 @@
 				<Button type="submit" class="ml-2">Search</Button>
 			</div>
 
+			<h2
+				class="scroll-m-20 border-b pb-2 text-3xl font-semibold tracking-tight transition-colors first:mt-0"
+			>
+				Columns
+			</h2>
+
 			<div class="relative">
 				<div class="pointer-events-none absolute inset-y-0 left-0 flex items-center pl-3">
 					<List class="text-muted-foreground" />
@@ -82,59 +88,60 @@
 					<Select.Input name="selectedColumns" />
 				</Select.Root>
 			</div>
-			<Popover.Root>
-				<Popover.Trigger>Order By</Popover.Trigger>
-				<Popover.Content class="w-full max-w-xl">
-					{#each orderByConfig as orderByItem, i}
-						<div class="flex items-center">
-							<Select.Root
-								selected={{ label: orderByItem.column, value: orderByItem.column }}
-								onSelectedChange={(selected) => {
-									if (!selected) return;
-									orderByConfig[i].column = selected.value;
-								}}
-							>
-								<Select.Trigger class="w-full">
-									<Select.Value class="pl-7 truncate" placeholder="Select column" />
-								</Select.Trigger>
-								<Select.Content>
-									{#each data.allCourseColumnNames as courseColumnName (courseColumnName)}
-										<Select.Item value={courseColumnName}>
-											{courseColumnName}
-										</Select.Item>
-									{/each}
-								</Select.Content>
-								<Select.Input name="orderByConfig[{i}].column" />
-							</Select.Root>
-							<Select.Root
-								selected={{ label: orderByItem.direction, value: orderByItem.direction }}
-								onSelectedChange={(selected) => {
-									if (!selected) return;
-									orderByConfig[i].direction = selected.value;
-								}}
-							>
-								<Select.Trigger class="w-full">
-									<Select.Value class="pl-7 truncate" placeholder="Select direction" />
-								</Select.Trigger>
-								<Select.Content>
-									<Select.Item value="asc">Ascending</Select.Item>
-									<Select.Item value="desc">Descending</Select.Item>
-								</Select.Content>
-								<Select.Input name="orderByConfig[{i}].direction" />
-							</Select.Root>
-							<Button
-								variant="ghost"
-								size="icon"
-								on:click={() => {
-									orderByConfig = [...orderByConfig.slice(0, i), ...orderByConfig.slice(i + 1)];
-								}}
-							>
-								<Trash />
-							</Button>
-						</div>
-					{/each}
-				</Popover.Content>
-			</Popover.Root>
+
+			<h2
+				class="scroll-m-20 border-b pb-2 text-3xl font-semibold tracking-tight transition-colors first:mt-0"
+			>
+				Order By
+			</h2>
+			{#each orderByConfig as orderByItem, i}
+				<div class="flex items-center">
+					<Select.Root
+						selected={{ label: orderByItem.column, value: orderByItem.column }}
+						onSelectedChange={(selected) => {
+							if (!selected) return;
+							orderByConfig[i].column = selected.value;
+						}}
+					>
+						<Select.Trigger class="w-full">
+							<Select.Value class="pl-7 truncate" placeholder="Select column" />
+						</Select.Trigger>
+						<Select.Content>
+							{#each data.allCourseColumnNames as courseColumnName (courseColumnName)}
+								<Select.Item value={courseColumnName}>
+									{courseColumnName}
+								</Select.Item>
+							{/each}
+						</Select.Content>
+						<Select.Input name="orderByConfig[{i}].column" />
+					</Select.Root>
+					<Select.Root
+						selected={{ label: orderByItem.direction, value: orderByItem.direction }}
+						onSelectedChange={(selected) => {
+							if (!selected) return;
+							orderByConfig[i].direction = selected.value;
+						}}
+					>
+						<Select.Trigger class="w-full">
+							<Select.Value class="pl-7 truncate" placeholder="Select direction" />
+						</Select.Trigger>
+						<Select.Content>
+							<Select.Item value="asc">Ascending</Select.Item>
+							<Select.Item value="desc">Descending</Select.Item>
+						</Select.Content>
+						<Select.Input name="orderByConfig[{i}].direction" />
+					</Select.Root>
+					<Button
+						variant="ghost"
+						size="icon"
+						on:click={() => {
+							orderByConfig = [...orderByConfig.slice(0, i), ...orderByConfig.slice(i + 1)];
+						}}
+					>
+						<Trash />
+					</Button>
+				</div>
+			{/each}
 		</form>
 	</Collapsible.Root>
 	<!-- Add screenshot with rounded corners -->
